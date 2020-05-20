80e2a404473345bbd74335a8faf949e2c18e6026
jrfeng <jrfeng331@qq.com>
2020-05-20 08:03:19 +0800

    modify 'Player'
    - add methods:
        - setSoundQuality(int)
        - setAudioEffectEnabled(boolean)
        - setOnlyWifiNetwork(boolean)
        - setIgnoreLossAudioFocus(boolean)
    - add inner class:
        - SoundQuality
        - PlaybackState

--- player/src/main/java/snow/player/Player.java
+++ player/src/main/java/snow/player/Player.java
@@ -19,15 +19,30 @@ public interface Player {
 
     void rewind();
 
-    enum PlaybackState {
-        UNKNOWN,
-        PREPARING,
-        PREPARED,
-        PLAYING,
-        PAUSED,
-        STOPPED,
-        STALLED,
-        ERROR
+    void setSoundQuality(int soundQuality);
+
+    void setAudioEffectEnabled(boolean enabled);
+
+    void setOnlyWifiNetwork(boolean onlyWifiNetwork);
+
+    void setIgnoreLossAudioFocus(boolean ignoreLossAudioFocus);
+
+    class SoundQuality {
+        public static final int STANDARD = 0;
+        public static final int LOW = 1;
+        public static final int HIGH = 2;
+        public static final int LOSSLESS = 3;
+    }
+
+    class PlaybackState {
+        public static final int UNKNOWN = 0;
+        public static final int PREPARING = 1;
+        public static final int PREPARED = 2;
+        public static final int PLAYING = 3;
+        public static final int PAUSED = 4;
+        public static final int STOPPED = 5;
+        public static final int STALLED = 6;
+        public static final int ERROR = 7;
     }
 
     interface OnPlaybackStateChangeListener {
