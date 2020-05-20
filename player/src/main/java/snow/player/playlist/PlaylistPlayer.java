31bf1f11efddd031ac55f66bf3a501c6d5d14cbd
jrfeng <jrfeng331@qq.com>
2020-05-20 08:18:20 +0800

    add 'PlaylistPlayer'

---
+++ player/src/main/java/snow/player/playlist/PlaylistPlayer.java
@@ -0,0 +1,41 @@
+package snow.player.playlist;
+
+import java.util.List;
+
+import snow.player.Player;
+
+public interface PlaylistPlayer extends Player {
+    void skipToPrevious();
+
+    void skipToPosition(int position);
+
+    void setPlayMode(int playMode);
+
+    void notifyPlaylistSwapped();
+
+    void notifyMusicInserted(int position);
+
+    void notifyMusicInserted(int position, int count);
+
+    void notifyMusicRemoved(int position);
+
+    void notifyMusicRemoved(List<Integer> positions);
+
+    class PlayMode {
+        public static final int SEQUENTIAL = 0;
+        public static final int LOOP = 1;
+        public static final int SHUFFLE = 2;
+    }
+
+    interface OnPlaylistChangeListener {
+        void onPlaylistChanged(PlaylistManager playlistManager);
+    }
+
+    interface OnPlayModeChangeListener {
+        void onPlayModeChanged(int playMode);
+    }
+
+    interface OnPlayingMusicItemPositionChangeListener {
+        void onPlayingMusicItemPositionChanged(int position);
+    }
+}
