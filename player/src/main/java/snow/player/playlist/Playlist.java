8fa7693c3a178852846bdc00afe3a700fc80299b
jrfeng <jrfeng331@qq.com>
2020-05-21 15:52:50 +0800

    add docs

--- player/src/main/java/snow/player/playlist/Playlist.java
+++ player/src/main/java/snow/player/playlist/Playlist.java
@@ -26,22 +26,50 @@ public final class Playlist implements Iterable<MusicItem>, Parcelable {
         mMusicItems = new ArrayList<>(items);
     }
 
+    /**
+     * 如果当前播放队列包含指定的元素，则返回 true。
+     */
     public boolean contains(MusicItem musicItem) {
         return mMusicItems.contains(musicItem);
     }
 
+    /**
+     * 返回当前播放队列中指定位置的元素。
+     *
+     * @param index 要返回的元素的索引。
+     * @return 当前播放队列中指定位置的元素。
+     * @throws IndexOutOfBoundsException 如果索引超出范围 (index < 0 || index >= size())
+     */
     public MusicItem get(int index) throws IndexOutOfBoundsException {
         return mMusicItems.get(index);
     }
 
+    /**
+     * 返回当前播放队列中第一次出现的指定元素的索引；如果当前播放队列不包含该元素，则返回 -1。
+     *
+     * @param musicItem 要搜索的元素（不能为 null）
+     * @return 此当前播放队列中第一次出现的指定元素的索引，如果当前播放队列不包含该元素，则返回 -1
+     */
     public int indexOf(MusicItem musicItem) {
         return mMusicItems.indexOf(musicItem);
     }
 
+    /**
+     * 如果当前播放队列在没有任何元素，则返回 true。
+     *
+     * @return 如果列表不包含元素，则返回 true。
+     */
     public boolean isEmpty() {
         return mMusicItems.isEmpty();
     }
 
+    /**
+     * 返回按适当顺序在当前播放队列的元素上进行迭代的迭代器。
+     * <p>
+     * <b>注意！由于 Playlist 被设计为是不可变的，因此不允许使用 Iterator#remove() 方法来删除元素。</b>
+     *
+     * @return 按适当顺序在列表的元素上进行迭代的迭代器。
+     */
     @NonNull
     @Override
     public Iterator<MusicItem> iterator() {
@@ -65,14 +93,36 @@ public final class Playlist implements Iterable<MusicItem>, Parcelable {
         };
     }
 
+    /**
+     * 返回当前播放队列中最后出现的指定元素的索引；如果当前播放队列不包含此元素，则返回 -1。
+     * <p>
+     * 更确切地讲，返回满足 (o==null ? get(i)==null : o.equals(get(i))) 的最高索引 i；如果没有这样的索引，则返回 -1。
+     *
+     * @param musicItem 要搜索的元素（不能为 null）
+     * @return 列表中最后出现的指定元素的索引；如果列表不包含此元素，则返回 -1。
+     */
     public int lastIndexOf(MusicItem musicItem) {
         return mMusicItems.lastIndexOf(musicItem);
     }
 
+    /**
+     * 返回当前播放队列中的元素数。
+     * <p>
+     * 如果当前播放队列包含多于 Integer.MAX_VALUE 个元素，则返回 Integer.MAX_VALUE。
+     *
+     * @return 当前播放队列中的元素数
+     */
     public int size() {
         return mMusicItems.size();
     }
 
+    /**
+     * 获取当前播放队列中包含的所有 MusicItem 元素。
+     * <p>
+     * 如果当前播放队列为空，则返回一个空列表。
+     *
+     * @return 当前播放队列中包含的所有 MusicItem 元素。如果当前播放队列为空，则返回一个空列表。
+     */
     public List<MusicItem> getAllMusicItem() {
         return new ArrayList<>(mMusicItems);
     }
