ed5985416fa123228f4a30daf5615a242e9b4c37
jrfeng <jrfeng331@qq.com>
2020-05-21 14:52:41 +0800

    modify: 'PlaylistManager'
    - add methods:
        - newPlaylist(int,boolean):PlaylistEditor
        - editPlaylist():PlaylistEditor
    - add static inner class:
        - PlaylistEditor

--- player/src/main/java/snow/player/playlist/PlaylistManager.java
+++ player/src/main/java/snow/player/playlist/PlaylistManager.java
@@ -1,4 +1,21 @@
 package snow.player.playlist;
 
 public class PlaylistManager {
+
+    public PlaylistEditor newPlaylist(int position, boolean playOnPrepared) {
+        // TODO
+        return null;
+    }
+
+    public PlaylistEditor editPlaylist() {
+        // TODO
+        return null;
+    }
+
+    public static class PlaylistEditor {
+
+        public void apply() {
+
+        }
+    }
 }
