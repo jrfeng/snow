639b692bc1362f6bb2a5c76b6f411a01849200d0
jrfeng <jrfeng331@qq.com>
2020-06-03 12:11:08 +0800

    update 'PlaylistStateTest'

---
+++ player/src/androidTest/java/snow/player/PlaylistStateTest.java
@@ -0,0 +1,98 @@
+package snow.player;
+
+import android.os.Parcel;
+
+import org.junit.Test;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+import snow.player.playlist.PlaylistPlayer;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class PlaylistStateTest {
+
+    @Test
+    public void constructor1Test() {
+        PlaylistState ps = new PlaylistState();
+
+        assertEquals(0, ps.getPosition());
+        assertEquals(PlaylistPlayer.PlayMode.SEQUENTIAL, ps.getPlayMode());
+    }
+
+    @Test
+    public void constructor2Test() {
+        PlaylistState ps = new PlaylistState();
+        ps.setPosition(50);
+        ps.setPlayMode(PlaylistPlayer.PlayMode.SHUFFLE);
+
+        PlaylistState ps2 = new PlaylistState(ps);
+
+        assertEquals(ps, ps2);
+    }
+
+    @Test
+    public void setPosition() {
+        final int position = -5;
+
+        PlaylistState ps = new PlaylistState();
+        ps.setPosition(position);
+
+        assertEquals(0, ps.getPosition());
+    }
+
+    @Test
+    public void equals_hashCode() {
+        final int position = 50;
+        final int playMode = PlaylistPlayer.PlayMode.LOOP;
+
+        PlaylistState ps1 = new PlaylistState();
+        ps1.setPosition(position);
+        ps1.setPlayMode(playMode);
+
+        PlaylistState ps2 = new PlaylistState();
+        ps2.setPosition(position);
+        ps2.setPlayMode(playMode);
+
+        assertEquals(ps1, ps2);
+        assertEquals(ps1.hashCode(), ps2.hashCode());
+    }
+
+    @Test
+    public void cloneTest() throws CloneNotSupportedException {
+        final int position = 50;
+        final int playMode = PlaylistPlayer.PlayMode.LOOP;
+
+        PlaylistState ps1 = new PlaylistState();
+        ps1.setPosition(position);
+        ps1.setPlayMode(playMode);
+
+        PlaylistState ps2 = ps1.clone();
+
+        assertEquals(ps1, ps2);
+    }
+
+    @Test
+    public void parcelableTest() {
+        Parcel parcel = Parcel.obtain();
+
+        final int position = 50;
+        final int playMode = PlaylistPlayer.PlayMode.LOOP;
+
+        PlaylistState ps1 = new PlaylistState();
+        ps1.setPosition(position);
+        ps1.setPlayMode(playMode);
+
+        ps1.writeToParcel(parcel, 0);
+
+        parcel.setDataPosition(0);
+        PlaylistState ps2 = new PlaylistState(parcel);
+
+        assertEquals(ps1, ps2);
+
+        parcel.recycle();
+    }
+}
