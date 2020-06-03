0f8a68735fc64e6503c57c6ad63dde48258ab32b
jrfeng <jrfeng331@qq.com>
2020-06-03 10:22:21 +0800

    add 'RadioStationTest'

---
+++ player/src/androidTest/java/snow/player/radio/RadioStationTest.java
@@ -0,0 +1,87 @@
+package snow.player.radio;
+
+import android.os.Bundle;
+import android.os.Parcel;
+
+import org.junit.Test;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import snow.player.MusicItem;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class RadioStationTest {
+    private final String id = "id_1234";
+    private final String name = "test__hello";
+    private final String description = "test_description";
+
+    @Test
+    public void constructor1Test() {
+        RadioStation rs = new RadioStation();
+
+        assertEquals("", rs.getId());
+        assertEquals("unknown", rs.getName());
+        assertEquals("", rs.getDescription());
+    }
+
+    @Test
+    public void constructor2Test() {
+        RadioStation rs = new RadioStation(id, name, description);
+
+        assertEquals(id, rs.getId());
+        assertEquals(name, rs.getName());
+        assertEquals(description, rs.getDescription());
+    }
+
+    @Test
+    public void equalsTest() {
+        RadioStation rs1 = new RadioStation(id, name, description);
+        RadioStation rs2 = new RadioStation(id, name, description);
+
+        assertEquals(rs1, rs2);
+
+        Bundle extra1 = new Bundle();
+        extra1.putString("extra1", "value1");
+
+        Bundle extra2 = new Bundle();
+        extra2.putString("extra2", "value2");
+
+        rs1.setExtra(extra1);
+        rs2.setExtra(extra2);
+
+        assertEquals(rs1, rs2);
+    }
+
+    @Test
+    public void hashCodeTest() {
+        RadioStation rs1 = new RadioStation(id, name, description);
+        RadioStation rs2 = new RadioStation(id, name, description);
+
+        assertEquals(rs1, rs2);
+        assertEquals(rs1.hashCode(), rs2.hashCode());
+    }
+
+    @Test
+    public void parcelableTest() {
+        Parcel parcel = Parcel.obtain();
+
+        RadioStation rs = new RadioStation(id, name, description);
+
+        rs.writeToParcel(parcel, 0);
+
+        parcel.setDataPosition(0);
+
+        RadioStation other = new RadioStation(parcel);
+
+        assertEquals(rs, other);
+
+        parcel.recycle();
+    }
+}
