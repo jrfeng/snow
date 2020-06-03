7a33addc87ef4a8e8195ae79cf164a326efaf227
jrfeng <jrfeng331@qq.com>
2020-06-03 11:24:01 +0800

    update 'PlayerStateTest'

--- player/src/androidTest/java/snow/player/PlayerStateTest.java
+++ player/src/androidTest/java/snow/player/PlayerStateTest.java
@@ -17,6 +17,7 @@ public class PlayerStateTest {
     public void constructorTest() {
         PlayerState playerState = new PlayerState();
 
+        assertEquals(0, playerState.getPlayProgress());
         assertEquals(Player.PlaybackState.UNKNOWN, playerState.getPlaybackState());
         assertEquals(Player.SoundQuality.STANDARD, playerState.getSoundQuality());
         assertFalse(playerState.isAudioEffectEnabled());
@@ -24,17 +25,39 @@ public class PlayerStateTest {
         assertFalse(playerState.isIgnoreLossAudioFocus());
     }
 
-    @Test
-    public void equals_hashCode() {
+    public void constructor2Test() {
         PlayerState playerState = new PlayerState();
-
-        playerState.setPlaybackState(Player.PlaybackState.PLAYING);
-        playerState.setSoundQuality(Player.SoundQuality.SUPER);
-        playerState.setAudioEffectEnabled(false);
-        playerState.setOnlyWifiNetwork(true);
-        playerState.setIgnoreLossAudioFocus(false);
+        playerState.setPlayProgress(1000);
 
         PlayerState other = new PlayerState(playerState);
+
+        assertEquals(playerState, other);
+    }
+
+    @Test
+    public void equals_hashCode() {
+        final long playProgress = 1000;
+        final int playbackState = Player.PlaybackState.PLAYING;
+        final int soundQuality = Player.SoundQuality.SUPER;
+        final boolean audioEffectEnable = false;
+        final boolean onlyWifiNetwork = true;
+        final boolean ignoreAudioFocus = false;
+
+        PlayerState playerState = new PlayerState();
+        playerState.setPlayProgress(playProgress);
+        playerState.setPlaybackState(playbackState);
+        playerState.setSoundQuality(soundQuality);
+        playerState.setAudioEffectEnabled(audioEffectEnable);
+        playerState.setOnlyWifiNetwork(onlyWifiNetwork);
+        playerState.setIgnoreLossAudioFocus(ignoreAudioFocus);
+
+        PlayerState other = new PlayerState();
+        other.setPlayProgress(playProgress);
+        other.setPlaybackState(playbackState);
+        other.setSoundQuality(soundQuality);
+        other.setAudioEffectEnabled(audioEffectEnable);
+        other.setOnlyWifiNetwork(onlyWifiNetwork);
+        other.setIgnoreLossAudioFocus(ignoreAudioFocus);
 
         assertEquals(playerState, other);
         assertEquals(playerState.hashCode(), other.hashCode());
@@ -44,12 +67,14 @@ public class PlayerStateTest {
     public void parcelableTest() {
         PlayerState playerState = new PlayerState();
 
-        final int playbackState = Player.PlaybackState.PLAYING;
-        final int soundQuality = Player.SoundQuality.SUPER;
+        final long playProgress = 2000;
+        final int playbackState = Player.PlaybackState.PAUSED;
+        final int soundQuality = Player.SoundQuality.HIGH;
         final boolean audioEffectEnable = true;
         final boolean onlyWifiNetwork = false;
         final boolean ignoreAudioFocus = true;
 
+        playerState.setPlayProgress(playProgress);
         playerState.setPlaybackState(playbackState);
         playerState.setSoundQuality(soundQuality);
         playerState.setAudioEffectEnabled(audioEffectEnable);
