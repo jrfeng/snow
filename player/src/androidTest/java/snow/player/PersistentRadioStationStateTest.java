4e73e269aeeabf4b2d9f0be9233abac64cb2c0b3
jrfeng <jrfeng331@qq.com>
2020-06-03 16:09:26 +0800

    add 'PersistentRadioStationStateTest'

---
+++ player/src/androidTest/java/snow/player/PersistentRadioStationStateTest.java
@@ -0,0 +1,40 @@
+package snow.player;
+
+import android.content.Context;
+
+import org.junit.Test;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.runner.RunWith;
+
+import snow.player.playlist.PlaylistPlayer;
+import snow.player.radio.RadioStation;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class PersistentRadioStationStateTest {
+
+    @Test
+    public void constructorTest() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        String id = "test_persistent_radio_station_state";
+
+        RadioStation radioStation = new RadioStation("test_id", "test_name", "test_description");
+
+        PersistentRadioStationState prss = new PersistentRadioStationState(context, id);
+
+        prss.setPlayProgress(100);
+        prss.setSoundQuality(Player.SoundQuality.HIGH);
+        prss.setAudioEffectEnabled(true);
+        prss.setOnlyWifiNetwork(false);
+        prss.setIgnoreLossAudioFocus(false);
+        prss.setRadioStation(radioStation);
+
+        PersistentRadioStationState prss2 = new PersistentRadioStationState(context, id);
+
+        assertEquals(prss, prss2);
+    }
+}
