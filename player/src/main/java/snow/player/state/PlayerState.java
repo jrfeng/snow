d7eb2ae74e0a70351e8c0b8a973a30d05ea0d976
jrfeng <jrfeng331@qq.com>
2020-06-11 07:47:15 +0800

    modify 'PlayerState'
    - add 5 field:
        - mAudioSessionId
        - mBufferingPercent
        - mBufferingPercentUpdateTime
        - mErrorCode
        - mErrorMessage

--- player/src/main/java/snow/player/state/PlayerState.java
+++ player/src/main/java/snow/player/state/PlayerState.java
@@ -1,5 +1,6 @@
 package snow.player.state;
 
+import android.content.Context;
 import android.os.Parcel;
 import android.os.Parcelable;
 
@@ -7,9 +8,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
 
 import snow.player.MusicItem;
 import snow.player.Player;
+import snow.player.util.ErrorUtil;
 
 /**
  * 用于保存基本的播放器状态。
@@ -18,13 +21,20 @@ public class PlayerState implements Parcelable, Cloneable {
     private long mPlayProgress;
     private long mPlayProgressUpdateTime;
     private boolean mLooping;
-    private int mPlaybackState;
     private int mSoundQuality;
     private boolean mAudioEffectEnabled;
     private boolean mOnlyWifiNetwork;
     private boolean mIgnoreLossAudioFocus;
     @Nullable
     private MusicItem mMusicItem;
+
+    // no persistent
+    private int mPlaybackState;
+    private int mAudioSessionId;
+    private int mBufferingPercent;
+    private long mBufferingPercentUpdateTime;
+    private int mErrorCode;
+    private String mErrorMessage;
 
     public PlayerState() {
         mPlayProgress = 0;
@@ -35,18 +45,28 @@ public class PlayerState implements Parcelable, Cloneable {
         mAudioEffectEnabled = false;
         mOnlyWifiNetwork = true;
         mIgnoreLossAudioFocus = false;
+        mErrorCode = ErrorUtil.ERROR_NO_ERROR;
+        mErrorMessage = "";
     }
 
     public PlayerState(PlayerState source) {
         mPlayProgress = source.mPlayProgress;
         mPlayProgressUpdateTime = source.mPlayProgressUpdateTime;
         mLooping = source.mLooping;
-        mPlaybackState = source.mPlaybackState;
         mSoundQuality = source.mSoundQuality;
         mAudioEffectEnabled = source.mAudioEffectEnabled;
         mOnlyWifiNetwork = source.mOnlyWifiNetwork;
         mIgnoreLossAudioFocus = source.mIgnoreLossAudioFocus;
-        mMusicItem = new MusicItem(source.mMusicItem);
+        if (source.mMusicItem != null) {
+            mMusicItem = new MusicItem(source.mMusicItem);
+        }
+
+        mPlaybackState = source.mPlaybackState;
+        mAudioSessionId = source.mAudioSessionId;
+        mBufferingPercent = source.mBufferingPercent;
+        mBufferingPercentUpdateTime = source.mBufferingPercentUpdateTime;
+        mErrorCode = source.mErrorCode;
+        mErrorMessage = source.mErrorMessage;
     }
 
     /**
@@ -105,33 +125,6 @@ public class PlayerState implements Parcelable, Cloneable {
      */
     public void setLooping(boolean looping) {
         mLooping = looping;
-    }
-
-    /**
-     * 获取播放状态。
-     *
-     * @return 返回当前播放状态。
-     * @see Player.PlaybackState
-     */
-    public int getPlaybackState() {
-        return mPlaybackState;
-    }
-
-    /**
-     * 设置播放状态。
-     *
-     * @param playbackState 要设置的播放状态。只能为这些值之一：{@link Player.PlaybackState#UNKNOWN},
-     *                      {@link Player.PlaybackState#PREPARING},
-     *                      {@link Player.PlaybackState#PREPARED},
-     *                      {@link Player.PlaybackState#PLAYING},
-     *                      {@link Player.PlaybackState#PAUSED},
-     *                      {@link Player.PlaybackState#STOPPED},
-     *                      {@link Player.PlaybackState#STALLED},
-     *                      {@link Player.PlaybackState#ERROR}
-     * @see Player.PlaybackState
-     */
-    public void setPlaybackState(int playbackState) {
-        mPlaybackState = playbackState;
     }
 
     /**
@@ -226,6 +219,131 @@ public class PlayerState implements Parcelable, Cloneable {
         mMusicItem = musicItem;
     }
 
+    /**
+     * 获取播放状态。
+     *
+     * @return 返回当前播放状态。
+     * @see Player.PlaybackState
+     */
+    public int getPlaybackState() {
+        return mPlaybackState;
+    }
+
+    /**
+     * 设置播放状态。
+     *
+     * @param playbackState 要设置的播放状态。只能为这些值之一：{@link Player.PlaybackState#UNKNOWN},
+     *                      {@link Player.PlaybackState#PREPARING},
+     *                      {@link Player.PlaybackState#PREPARED},
+     *                      {@link Player.PlaybackState#PLAYING},
+     *                      {@link Player.PlaybackState#PAUSED},
+     *                      {@link Player.PlaybackState#STOPPED},
+     *                      {@link Player.PlaybackState#STALLED},
+     *                      {@link Player.PlaybackState#ERROR}
+     * @see Player.PlaybackState
+     */
+    public void setPlaybackState(int playbackState) {
+        mPlaybackState = playbackState;
+    }
+
+    /**
+     * 获取当前正在播放的音乐的 audio session id。
+     * <p>
+     * 注意！可能会返回 {@link android.media.AudioManager#AUDIO_SESSION_ID_GENERATE}，表示当前音乐的
+     * audio session id 不可用。
+     */
+    public int getAudioSessionId() {
+        return mAudioSessionId;
+    }
+
+    /**
+     * 设置当前音乐的 audio session id。
+     *
+     * @param audioSessionId 当前音乐的 audio session id。如果当前音乐的 audio session id 不可用，则可以
+     *                       设为 {@link android.media.AudioManager#AUDIO_SESSION_ID_GENERATE}。
+     */
+    public void setAudioSessionId(int audioSessionId) {
+        mAudioSessionId = audioSessionId;
+    }
+
+    /**
+     * 获取当前音乐的缓存进度。百分比值，范围为 [0 ~ 100]。
+     */
+    public int getBufferingPercent() {
+        return mBufferingPercent;
+    }
+
+    /**
+     * 设置当前音乐的缓存进度。百分比值，范围为 [0 ~ 100]。
+     */
+    public void setBufferingPercent(int bufferingPercent) {
+        if (bufferingPercent < 0) {
+            mBufferingPercent = 0;
+            return;
+        }
+
+        if (bufferingPercent > 100) {
+            mBufferingPercent = 100;
+            return;
+        }
+
+        mBufferingPercent = bufferingPercent;
+    }
+
+    /**
+     * 获取上一次缓存进度的更新时间。
+     */
+    public long getBufferingPercentUpdateTime() {
+        return mBufferingPercentUpdateTime;
+    }
+
+    /**
+     * 设置上一次缓存进度的更新时间。
+     */
+    public void setBufferingPercentUpdateTime(long bufferingPercentUpdateTime) {
+        mBufferingPercentUpdateTime = bufferingPercentUpdateTime;
+    }
+
+    /**
+     * 获取错误码。如果没有发生任何错误，则返回 {@link ErrorUtil#ERROR_NO_ERROR}。
+     *
+     * @see ErrorUtil
+     */
+    public int getErrorCode() {
+        return mErrorCode;
+    }
+
+    /**
+     * 设置错误码。
+     *
+     * @param errorCode 错误码。预定义的错误码：{@link ErrorUtil#ERROR_NO_ERROR},
+     *                  {@link ErrorUtil#ERROR_ONLY_WIFI_NETWORK},
+     *                  {@link ErrorUtil#ERROR_PLAYER_ERROR},
+     *                  {@link ErrorUtil#ERROR_NETWORK_UNAVAILABLE}
+     * @see ErrorUtil
+     */
+    public void setErrorCode(int errorCode) {
+        mErrorCode = errorCode;
+    }
+
+    /**
+     * 获取错误信息。
+     */
+    public String getErrorMessage() {
+        return mErrorMessage;
+    }
+
+    /**
+     * 设置错误信息（不能为 null）。
+     *
+     * @param errorMessage 错误信息（不能为 null）。
+     * @see ErrorUtil#getErrorMessage(Context, int)
+     */
+    public void setErrorMessage(@NonNull String errorMessage) {
+        Preconditions.checkNotNull(errorMessage);
+        mErrorMessage = errorMessage;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -236,12 +354,17 @@ public class PlayerState implements Parcelable, Cloneable {
         return Objects.equal(mPlayProgress, other.mPlayProgress)
                 && Objects.equal(mPlayProgressUpdateTime, other.mPlayProgressUpdateTime)
                 && Objects.equal(mLooping, other.mLooping)
-                && Objects.equal(mPlaybackState, other.mPlaybackState)
                 && Objects.equal(mSoundQuality, other.mSoundQuality)
                 && Objects.equal(mAudioEffectEnabled, other.mAudioEffectEnabled)
                 && Objects.equal(mOnlyWifiNetwork, other.mOnlyWifiNetwork)
                 && Objects.equal(mIgnoreLossAudioFocus, other.mIgnoreLossAudioFocus)
-                && Objects.equal(mMusicItem, other.mMusicItem);
+                && Objects.equal(mMusicItem, other.mMusicItem)
+                && Objects.equal(mPlaybackState, other.mPlaybackState)
+                && Objects.equal(mAudioSessionId, other.mAudioSessionId)
+                && Objects.equal(mBufferingPercent, other.mBufferingPercent)
+                && Objects.equal(mBufferingPercentUpdateTime, other.mBufferingPercentUpdateTime)
+                && Objects.equal(mErrorCode, other.mErrorCode)
+                && Objects.equal(mErrorMessage, other.mErrorMessage);
     }
 
     @Override
@@ -249,12 +372,17 @@ public class PlayerState implements Parcelable, Cloneable {
         return Objects.hashCode(mPlayProgress,
                 mPlayProgressUpdateTime,
                 mLooping,
-                mPlaybackState,
                 mSoundQuality,
                 mAudioEffectEnabled,
                 mOnlyWifiNetwork,
                 mIgnoreLossAudioFocus,
-                mMusicItem);
+                mMusicItem,
+                mPlaybackState,
+                mAudioSessionId,
+                mBufferingPercent,
+                mBufferingPercentUpdateTime,
+                mErrorCode,
+                mErrorMessage);
     }
 
     @NonNull
@@ -268,12 +396,18 @@ public class PlayerState implements Parcelable, Cloneable {
         mPlayProgress = in.readLong();
         mPlayProgressUpdateTime = in.readLong();
         mLooping = in.readByte() != 0;
-        mPlaybackState = in.readInt();
         mSoundQuality = in.readInt();
         mAudioEffectEnabled = in.readByte() != 0;
         mOnlyWifiNetwork = in.readByte() != 0;
         mIgnoreLossAudioFocus = in.readByte() != 0;
         mMusicItem = in.readParcelable(Thread.currentThread().getContextClassLoader());
+
+        mPlaybackState = in.readInt();
+        mAudioSessionId = in.readInt();
+        mBufferingPercent = in.readInt();
+        mBufferingPercentUpdateTime = in.readLong();
+        mErrorCode = in.readInt();
+        mErrorMessage = in.readString();
     }
 
     @Override
@@ -281,12 +415,18 @@ public class PlayerState implements Parcelable, Cloneable {
         dest.writeLong(mPlayProgress);
         dest.writeLong(mPlayProgressUpdateTime);
         dest.writeByte((byte) (mLooping ? 1 : 0));
-        dest.writeInt(mPlaybackState);
         dest.writeInt(mSoundQuality);
         dest.writeByte((byte) (mAudioEffectEnabled ? 1 : 0));
         dest.writeByte((byte) (mOnlyWifiNetwork ? 1 : 0));
         dest.writeByte((byte) (mIgnoreLossAudioFocus ? 1 : 0));
         dest.writeParcelable(mMusicItem, flags);
+
+        dest.writeInt(mPlaybackState);
+        dest.writeInt(mAudioSessionId);
+        dest.writeInt(mBufferingPercent);
+        dest.writeLong(mBufferingPercentUpdateTime);
+        dest.writeInt(mErrorCode);
+        dest.writeString(mErrorMessage);
     }
 
     @Override
