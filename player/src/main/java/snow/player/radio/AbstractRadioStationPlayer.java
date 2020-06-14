0e15d6f83e9cc9f030d78fe5b0eb6e483d03d770
jrfeng <jrfeng331@qq.com>
2020-06-14 09:46:59 +0800

    optimize 'AbstractRadioStationPlayer'

--- player/src/main/java/snow/player/radio/AbstractRadioStationPlayer.java
+++ player/src/main/java/snow/player/radio/AbstractRadioStationPlayer.java
@@ -6,6 +6,13 @@ import androidx.annotation.NonNull;
 
 import java.util.HashMap;
 
+import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
+import io.reactivex.SingleOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 import snow.player.AbstractPlayer;
 import snow.player.MusicItem;
 import snow.player.state.RadioStationState;
@@ -14,6 +21,8 @@ import snow.player.state.RadioStationStateListener;
 public abstract class AbstractRadioStationPlayer extends AbstractPlayer<RadioStationStateListener>
         implements RadioStationPlayer {
     private RadioStationState mRadioStationState;
+
+    private Disposable mDisposable;
 
     public AbstractRadioStationPlayer(@NonNull Context context, @NonNull RadioStationState radioStationState) {
         super(context, radioStationState);
@@ -34,17 +43,61 @@ public abstract class AbstractRadioStationPlayer extends AbstractPlayer<RadioSta
         }
     }
 
+    /**
+     * 获取 “电台” 的下一首音乐。
+     * <p>
+     * 该方法会在异步线程中调用。
+     *
+     * @param radioStation 用于表示电台的 RadioStation 对象
+     * @return “电台” 的下一首音乐（不能为 null）
+     */
     @NonNull
     protected abstract MusicItem getNextMusicItem(RadioStation radioStation);
 
+    private Single<MusicItem> getNextMusicItemAsync(final RadioStation radioStation) {
+        return Single.create(new SingleOnSubscribe<MusicItem>() {
+            @Override
+            public void subscribe(SingleEmitter<MusicItem> emitter) throws Exception {
+                MusicItem musicItem = getNextMusicItem(radioStation);
+
+                if (emitter.isDisposed()) {
+                    return;
+                }
+
+                emitter.onSuccess(musicItem);
+            }
+        });
+    }
+
+    @Override
+    protected void onRelease() {
+        disposeLastGetNextMusicItem();
+    }
+
+    private void disposeLastGetNextMusicItem() {
+        if (mDisposable != null) {
+            mDisposable.dispose();
+        }
+    }
+
     @Override
     public void skipToNext() {
-        notifyPlayingMusicItemChanged(getNextMusicItem(mRadioStationState.getRadioStation()), true);
+        disposeLastGetNextMusicItem();
+
+        mDisposable = getNextMusicItemAsync(mRadioStationState.getRadioStation())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<MusicItem>() {
+                    @Override
+                    public void accept(MusicItem musicItem) throws Exception {
+                        notifyPlayingMusicItemChanged(musicItem, true);
+                    }
+                });
     }
 
     @Override
     public void setRadioStation(RadioStation radioStation) {
         notifyRadioStationChanged(radioStation);
-        notifyPlayingMusicItemChanged(getNextMusicItem(mRadioStationState.getRadioStation()), true);
+        skipToNext();
     }
 }
