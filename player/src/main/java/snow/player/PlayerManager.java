7f13466ce6312332b9fe2208ded4590e1feb76f5
jrfeng <jrfeng331@qq.com>
2020-06-14 09:03:46 +0800

    modify 'PlayerManager'
    - remove method: setPlayerType(int)
    - add interface: OnConfigChangeListener

--- player/src/main/java/snow/player/PlayerManager.java
+++ player/src/main/java/snow/player/PlayerManager.java
@@ -3,6 +3,8 @@ package snow.player;
 import android.os.IBinder;
 
 import channel.helper.Channel;
+import snow.player.state.PlaylistState;
+import snow.player.state.RadioStationState;
 
 /**
  * 该接口定义了用于管理播放器的基本功能。
@@ -11,13 +13,6 @@ import channel.helper.Channel;
 public interface PlayerManager {
     int TYPE_PLAYLIST = 0;
     int TYPE_RADIO_STATION = 1;
-
-    /**
-     * 设置播放器的播放模式。
-     *
-     * @param type 播放器的播放模式。该参数的值应该是：{@link #TYPE_PLAYLIST}, {@link #TYPE_RADIO_STATION} 两者之一。
-     */
-    void setPlayerType(int type);
 
     /**
      * 注册一个播放器状态监听器。
@@ -33,4 +28,13 @@ public interface PlayerManager {
      * @param token 已注册的监听器的 token（不能为 null）。如果监听器没有注册或者已经取消注册，那么将忽略此操作。
      */
     void unregisterPlayerStateListener(String token);
+
+    @Channel
+    interface OnConfigChangeListener {
+        void onPlayerTypeChanged(int playerType);
+
+        void syncPlaylistState(PlaylistState playlistState);
+
+        void syncRadioStationState(RadioStationState radioStationState);
+    }
 }
