df0f8911fb5cce581a4cdec98f848fe29950a5fb
jrfeng <jrfeng331@qq.com>
2020-06-14 09:51:12 +0800

    modify 'PlayerService'

--- player/src/main/java/snow/player/PlayerService.java
+++ player/src/main/java/snow/player/PlayerService.java
@@ -1,15 +1,417 @@
 package snow.player;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.IBinder;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class PlayerService extends Service {
+import com.google.common.base.Preconditions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import channel.helper.Dispatcher;
+import channel.helper.pipe.MessengerPipe;
+import snow.player.playlist.AbstractPlaylistPlayer;
+import snow.player.playlist.PlaylistManager;
+import snow.player.playlist.PlaylistPlayerChannel;
+import snow.player.radio.AbstractRadioStationPlayer;
+import snow.player.radio.RadioStation;
+import snow.player.radio.RadioStationPlayerChannel;
+import snow.player.state.PersistentPlaylistState;
+import snow.player.state.PersistentRadioStationState;
+import snow.player.state.PlaylistState;
+import snow.player.state.PlaylistStateListenerChannel;
+import snow.player.state.RadioStationState;
+import snow.player.state.RadioStationStateListenerChannel;
+
+public abstract class PlayerService extends Service implements PlayerManager {
+    private PlaylistState mPlaylistState;
+    private RadioStationState mRadioStationState;
+
+    private PlaylistManager mPlaylistManager;
+
+    private PlaylistPlayerImp mPlaylistPlayer;
+    private RadioStationPlayerImp mRadioStationPlayer;
+
+    private MessengerPipe mControllerPipe;
+
+    private HashMap<String, OnConfigChangeListener> mConfigChangeListenerMap;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        startService(new Intent(this, this.getClass()));
+
+        mConfigChangeListenerMap = new HashMap<>();
+
+        initPlayerState();
+        initPlaylistManager();
+        initPlayer();
+        initControllerPipe();
+    }
+
+    private void initPlayerState() {
+        String id = this.getClass().getName();
+
+        mPlaylistState = new PersistentPlaylistState(this, id);
+        mRadioStationState = new PersistentRadioStationState(this, id);
+    }
+
+    private void initPlaylistManager() {
+        mPlaylistManager = PlaylistManager.newInstance(this, this.getClass().getName());
+    }
+
+    private void initPlayer() {
+        mPlaylistPlayer = new PlaylistPlayerImp(this, mPlaylistState, mPlaylistManager);
+        mRadioStationPlayer = new RadioStationPlayerImp(this, mRadioStationState);
+    }
+
+    private void initControllerPipe() {
+        final PlayerManagerChannel.Dispatcher playerManagerDispatcher
+                = new PlayerManagerChannel.Dispatcher(this);
+
+        final PlaylistPlayerChannel.Dispatcher playlistDispatcher =
+                new PlaylistPlayerChannel.Dispatcher(mPlaylistPlayer);
+
+        final RadioStationPlayerChannel.Dispatcher radioStationDispatcher =
+                new RadioStationPlayerChannel.Dispatcher(mRadioStationPlayer);
+
+        mControllerPipe = new MessengerPipe(new Dispatcher() {
+            @Override
+            public boolean dispatch(Map<String, Object> data) {
+                if (playerManagerDispatcher.dispatch(data)) {
+                    return true;
+                }
+
+                if (playlistDispatcher.dispatch(data)) {
+                    notifyPlayerTypeChanged(PlayerManager.TYPE_PLAYLIST);
+                    return true;
+                }
+
+                if (radioStationDispatcher.dispatch(data)) {
+                    notifyPlayerTypeChanged(PlayerManager.TYPE_RADIO_STATION);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void notifyPlayerTypeChanged(int playerType) {
+        for (String key : mConfigChangeListenerMap.keySet()) {
+            OnConfigChangeListener listener = mConfigChangeListenerMap.get(key);
+            if (listener != null) {
+                listener.onPlayerTypeChanged(playerType);
+            }
+        }
+    }
+
+    private void syncPlayerState(OnConfigChangeListener listener) {
+        listener.syncPlayerState(new PlaylistState(mPlaylistState), new RadioStationState(mRadioStationState));
+    }
+
+    private void addOnConfigChangeListener(@NonNull String token, @NonNull OnConfigChangeListener listener) {
+        Preconditions.checkNotNull(token);
+        Preconditions.checkNotNull(listener);
+
+        mConfigChangeListenerMap.put(token, listener);
+        syncPlayerState(listener);
+    }
+
+    private void removeOnConfigChangeListener(@NonNull String token) {
+        Preconditions.checkNotNull(token);
+
+        mConfigChangeListenerMap.remove(token);
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mControllerPipe.getBinder();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mPlaylistPlayer.release();
+        mRadioStationPlayer.release();
+
+        mPlaylistPlayer = null;
+        mRadioStationPlayer = null;
+    }
+
+    @Override
+    public void registerPlayerStateListener(String token, IBinder listener) {
+        MessengerPipe pipe = new MessengerPipe(listener);
+
+        addOnConfigChangeListener(token, new OnConfigChangeListenerChannel.Emitter(pipe));
+        mPlaylistPlayer.addStateListener(token, new PlaylistStateListenerChannel.Emitter(pipe));
+        mRadioStationPlayer.addStateListener(token, new RadioStationStateListenerChannel.Emitter(pipe));
+    }
+
+    @Override
+    public void unregisterPlayerStateListener(String token) {
+        removeOnConfigChangeListener(token);
+        mPlaylistPlayer.removeStateListener(token);
+        mRadioStationPlayer.removeStateListener(token);
+    }
+
+    /**
+     * 查询具有 soundQuality 音质的 MusicItem 表示的的音乐是否已被缓存。
+     * <p>
+     * 该方法会在异步线程中被调用。
+     *
+     * @param musicItem    要查询的 MusicItem 对象
+     * @param soundQuality 音乐的音质
+     * @return 如果已被缓存，则返回 true，否则返回 false
+     */
+    protected abstract boolean isCached(MusicItem musicItem, int soundQuality);
+
+    /**
+     * 获取已缓存的具有 soundQuality 音质的 MusicItem 表示的的音乐的 Uri。
+     *
+     * @param musicItem    MusicItem 对象
+     * @param soundQuality 音乐的音质
+     * @return 音乐的 Uri。可为 null，返回 null 时播放器会忽略本次播放。
+     */
+    @Nullable
+    protected abstract Uri getCachedUri(MusicItem musicItem, int soundQuality);
+
+    /**
+     * 从网络获取具有 soundQuality 音质的 MusicItem 表示的的音乐的 Uri。
+     * <p>
+     * 该方法会在异步线程中被调用。
+     *
+     * @param musicItem    MusicItem 对象
+     * @param soundQuality 音乐的音质
+     * @return 音乐的 Uri。可为 null，返回 null 时播放器会忽略本次播放。
+     */
+    @Nullable
+    protected abstract Uri getUri(MusicItem musicItem, int soundQuality);
+
+    /**
+     * 该方法会在创建 MusicPlayer 对象时调用。
+     * <p>
+     * 你可以重写该方法来返回你自己的 MusicPlayer 实现。
+     *
+     * @param uri 要播放的音乐的 uri
+     */
+    @NonNull
+    protected abstract MusicPlayer onCreateMusicPlayer(Uri uri);
+
+    /**
+     * 获取 “电台” 的下一首音乐。
+     * <p>
+     * 该方法会在异步线程中调用。
+     *
+     * @param radioStation 用于表示电台的 RadioStation 对象
+     * @return “电台” 的下一首音乐（不能为 null）
+     */
+    @NonNull
+    protected abstract MusicItem getNextMusicItem(RadioStation radioStation);
+
+    protected void onPreparing() {
+    }
+
+    protected void onPrepared(int audioSessionId) {
+    }
+
+    protected void onPlaying(long progress, long updateTime) {
+    }
+
+    protected void onPaused() {
+    }
+
+    protected void onStalled() {
+    }
+
+    protected void onStopped() {
+    }
+
+    protected void onError(int errorCode, String errorMessage) {
+    }
+
+    protected void onPlayComplete() {
+    }
+
+    protected void onRequestAudioFocus(boolean success) {
+    }
+
+    protected void onPlayingMusicItemChanged(@Nullable MusicItem musicItem) {
+    }
+
+    private class PlaylistPlayerImp extends AbstractPlaylistPlayer {
+
+        public PlaylistPlayerImp(@NonNull Context context,
+                                 @NonNull PlaylistState playlistState,
+                                 @NonNull PlaylistManager playlistManager) {
+            super(context, playlistState, playlistManager);
+        }
+
+        @Override
+        protected boolean isCached(MusicItem musicItem, int soundQuality) {
+            return PlayerService.this.isCached(musicItem, soundQuality);
+        }
+
+        @Nullable
+        @Override
+        protected Uri getCachedUri(MusicItem musicItem, int soundQuality) {
+            return PlayerService.this.getCachedUri(musicItem, soundQuality);
+        }
+
+        @Nullable
+        @Override
+        protected Uri getUri(MusicItem musicItem, int soundQuality) {
+            return PlayerService.this.getUri(musicItem, soundQuality);
+        }
+
+        @NonNull
+        @Override
+        protected MusicPlayer onCreateMusicPlayer(Uri uri) {
+            return PlayerService.this.onCreateMusicPlayer(uri);
+        }
+
+        @Override
+        protected void onPreparing() {
+            PlayerService.this.onPreparing();
+        }
+
+        @Override
+        protected void onPrepared(int audioSessionId) {
+            PlayerService.this.onPrepared(audioSessionId);
+        }
+
+        @Override
+        protected void onPlaying(long progress, long updateTime) {
+            PlayerService.this.onPlaying(progress, updateTime);
+        }
+
+        @Override
+        protected void onPaused() {
+            PlayerService.this.onPaused();
+        }
+
+        @Override
+        protected void onStalled() {
+            PlayerService.this.onStalled();
+        }
+
+        @Override
+        protected void onStopped() {
+            PlayerService.this.onStopped();
+        }
+
+        @Override
+        protected void onError(int errorCode, String errorMessage) {
+            PlayerService.this.onError(errorCode, errorMessage);
+        }
+
+        @Override
+        protected void onPlayComplete() {
+            PlayerService.this.onPlayComplete();
+        }
+
+        @Override
+        protected void onRequestAudioFocus(boolean success) {
+            PlayerService.this.onRequestAudioFocus(success);
+        }
+
+        @Override
+        protected void onPlayingMusicItemChanged(@Nullable MusicItem musicItem) {
+            PlayerService.this.onPlayingMusicItemChanged(musicItem);
+        }
+    }
+
+    private class RadioStationPlayerImp extends AbstractRadioStationPlayer {
+
+        public RadioStationPlayerImp(@NonNull Context context, @NonNull RadioStationState radioStationState) {
+            super(context, radioStationState);
+        }
+
+        @NonNull
+        @Override
+        protected MusicItem getNextMusicItem(RadioStation radioStation) {
+            return PlayerService.this.getNextMusicItem(radioStation);
+        }
+
+        @Override
+        protected boolean isCached(MusicItem musicItem, int soundQuality) {
+            return PlayerService.this.isCached(musicItem, soundQuality);
+        }
+
+        @Nullable
+        @Override
+        protected Uri getCachedUri(MusicItem musicItem, int soundQuality) {
+            return PlayerService.this.getCachedUri(musicItem, soundQuality);
+        }
+
+        @Nullable
+        @Override
+        protected Uri getUri(MusicItem musicItem, int soundQuality) {
+            return PlayerService.this.getUri(musicItem, soundQuality);
+        }
+
+        @NonNull
+        @Override
+        protected MusicPlayer onCreateMusicPlayer(Uri uri) {
+            return PlayerService.this.onCreateMusicPlayer(uri);
+        }
+
+        @Override
+        protected void onPreparing() {
+            PlayerService.this.onPreparing();
+        }
+
+        @Override
+        protected void onPrepared(int audioSessionId) {
+            PlayerService.this.onPrepared(audioSessionId);
+        }
+
+        @Override
+        protected void onPlaying(long progress, long updateTime) {
+            PlayerService.this.onPlaying(progress, updateTime);
+        }
+
+        @Override
+        protected void onPaused() {
+            PlayerService.this.onPaused();
+        }
+
+        @Override
+        protected void onStalled() {
+            PlayerService.this.onStalled();
+        }
+
+        @Override
+        protected void onStopped() {
+            PlayerService.this.onStopped();
+        }
+
+        @Override
+        protected void onError(int errorCode, String errorMessage) {
+            PlayerService.this.onError(errorCode, errorMessage);
+        }
+
+        @Override
+        protected void onPlayComplete() {
+            PlayerService.this.onPlayComplete();
+        }
+
+        @Override
+        protected void onRequestAudioFocus(boolean success) {
+            PlayerService.this.onRequestAudioFocus(success);
+        }
+
+        @Override
+        protected void onPlayingMusicItemChanged(@Nullable MusicItem musicItem) {
+            PlayerService.this.onPlayingMusicItemChanged(musicItem);
+        }
     }
 }
