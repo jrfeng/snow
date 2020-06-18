20eaa4cfe91a9a0c2f8a5a7d307b9330610e0cd7
jrfeng <jrfeng331@qq.com>
2020-06-18 13:23:52 +0800

    update docs

--- player/src/main/java/snow/player/PlayerManager.java
+++ player/src/main/java/snow/player/PlayerManager.java
@@ -31,14 +31,36 @@ public interface PlayerManager {
      */
     void unregisterPlayerStateListener(String token);
 
+    /**
+     * 用于监听播放器类型发生改变事件。
+     */
     interface OnPlayerTypeChangeListener {
+        /**
+         * 当播放器类型发生改变时会回调该方法。
+         *
+         * @param playerType 播放器类型。共有两个值：{@link #TYPE_PLAYLIST}：列表播放器；
+         *                   {@link #TYPE_RADIO_STATION}：电台播放器。
+         */
         void onPlayerTypeChanged(int playerType);
     }
 
+    /**
+     * 用于接收服务端发送的命令。
+     */
     @Channel
     interface OnCommandCallback extends OnPlayerTypeChangeListener {
+        /**
+         * 当服务端准备关闭时会回调该方法，此时客户端应主动断开与服务端的连接。
+         */
         void onShutdown();
 
+        /**
+         * 同步客户端与服务端的播放器状态。
+         *
+         * @param playerType        播放器类型
+         * @param playlistState     列表播放器的状态
+         * @param radioStationState 电台播放器的状态
+         */
         void syncPlayerState(int playerType, PlaylistState playlistState, RadioStationState radioStationState);
     }
 }
