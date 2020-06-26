d3f9c997e82c7fa2f43680fbb452ef84ecbdebc6
jrfeng <jrfeng331@qq.com>
2020-06-26 12:18:16 +0800

    add 'TestPlayer'

---
+++ player/src/androidTest/java/snow/player/test/TestPlayer.java
@@ -0,0 +1,93 @@
+package snow.player.test;
+
+import android.content.Context;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.io.IOException;
+
+import snow.player.AbstractPlayer;
+import snow.player.media.MusicItem;
+import snow.player.media.MusicPlayer;
+import snow.player.state.PlayerState;
+import snow.player.state.PlayerStateListener;
+
+public class TestPlayer extends AbstractPlayer<PlayerStateListener> {
+    private Tester mTester;
+
+    public TestPlayer(@NonNull Context context, @NonNull PlayerState playerState) {
+        super(context, playerState);
+
+        mTester = new Tester();
+    }
+
+    public Tester tester() {
+        return mTester;
+    }
+
+    public class Tester {
+        private boolean looping;
+        private boolean cached;
+        private TestMusicPlayer musicPlayer;
+
+        public void setTestMusicPlayer(TestMusicPlayer musicPlayer) {
+            this.musicPlayer = musicPlayer;
+        }
+
+        public void setLooping(boolean looping) {
+            this.looping = looping;
+        }
+
+        public void setCached(boolean cached) {
+            this.cached = cached;
+        }
+
+        public Uri getCachedUri() {
+            if (getMusicItem() == null) {
+                return null;
+            }
+
+            return Uri.parse(getMusicItem().getUri());
+        }
+
+        public Uri getUri() {
+            if (getMusicItem() == null) {
+                return null;
+            }
+
+            return Uri.parse(getMusicItem().getUri());
+        }
+    }
+
+    @Override
+    public boolean isLooping() {
+        return mTester.looping;
+    }
+
+    @Override
+    protected boolean isCached(MusicItem musicItem, SoundQuality soundQuality) {
+        return mTester.cached;
+    }
+
+    @Nullable
+    @Override
+    protected Uri getCachedUri(MusicItem musicItem, SoundQuality soundQuality) {
+        return mTester.getCachedUri();
+    }
+
+    @Nullable
+    @Override
+    protected Uri getUri(MusicItem musicItem, SoundQuality soundQuality) {
+        return mTester.getUri();
+    }
+
+    @NonNull
+    @Override
+    protected MusicPlayer onCreateMusicPlayer(Uri uri) throws IOException {
+        mTester.musicPlayer.reset();
+        mTester.musicPlayer.setDataSource(uri);
+        return mTester.musicPlayer;
+    }
+}
