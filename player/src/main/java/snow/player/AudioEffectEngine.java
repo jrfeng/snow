9337481aaf2670f82305902bb1eba17ab341e969
jrfeng <jrfeng331@qq.com>
2020-07-15 17:00:59 +0800

    add 'AudioEffectEngine'

---
+++ player/src/main/java/snow/player/AudioEffectEngine.java
@@ -0,0 +1,29 @@
+package snow.player;
+
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+/**
+ * 音频特效引擎。
+ */
+public interface AudioEffectEngine {
+    void init(@NonNull Config config);
+
+    void updateConfig(@NonNull Config config);
+
+    void attachAudioEffect(int audioSessionId);
+
+    void detachAudioEffect();
+
+    @NonNull
+    Config getDefaultConfig();
+
+    void release();
+
+    /**
+     * 音频特效引擎的配置信息。
+     */
+    interface Config extends Parcelable {
+    }
+}
