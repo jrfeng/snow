46f5e616db9395d244565b80d970a307d4f3dc3c
jrfeng <jrfeng331@qq.com>
2020-09-02 09:00:58 +0800

    optimize 'PlayerService.NotificationView'

--- player/src/main/java/snow/player/PlayerService.java
+++ player/src/main/java/snow/player/PlayerService.java
@@ -34,15 +34,27 @@ import androidx.media.session.MediaButtonReceiver;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners;
 import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.transition.Transition;
 import com.google.common.base.Preconditions;
 
+import java.io.InputStream;
 import java.lang.reflect.Method;
+import java.security.cert.CertificateException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import channel.helper.ChannelHelper;
 import channel.helper.Dispatcher;
@@ -52,6 +64,7 @@ import channel.helper.pipe.CustomActionPipe;
 import channel.helper.pipe.SessionEventPipe;
 import media.helper.HeadsetHookHelper;
 
+import okhttp3.OkHttpClient;
 import snow.player.appwidget.AppWidgetPreferences;
 import snow.player.effect.AudioEffectManager;
 import snow.player.media.MediaMusicPlayer;
@@ -1124,6 +1137,7 @@ public class PlayerService extends MediaBrowserServiceCompat implements PlayerMa
 
             setIconSize(playerService.getResources().getDimensionPixelSize(R.dimen.snow_notif_icon_size_big));
             setNeedReloadIcon(true);
+            initGlide();
             onInit(mPlayerService);
 
             mTarget = new CustomTarget<Bitmap>(mIconSize[0], mIconSize[1]) {
@@ -1137,6 +1151,64 @@ public class PlayerService extends MediaBrowserServiceCompat implements PlayerMa
                     // ignore
                 }
             };
+
+        }
+
+        private void initGlide() {
+            OkHttpClient.Builder clientBuilder = getUnsafeOkHttpClientBuilder()
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(20, TimeUnit.SECONDS);
+
+            Glide.get(mPlayerService)
+                    .getRegistry()
+                    .replace(GlideUrl.class,
+                            InputStream.class,
+                            new OkHttpUrlLoader.Factory(clientBuilder.build()));
+        }
+
+        private OkHttpClient.Builder getUnsafeOkHttpClientBuilder() {
+            try {
+                // Create a trust manager that does not validate certificate chains
+                final TrustManager[] trustAllCerts = new TrustManager[]{
+                        new X509TrustManager() {
+                            @Override
+                            public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                                // trust any client
+                            }
+
+                            @Override
+                            public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                                // trust any server
+                            }
+
+                            @Override
+                            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                                return new java.security.cert.X509Certificate[]{};
+                            }
+                        }
+                };
+
+                // Install the all-trusting trust manager
+                final SSLContext sslContext = SSLContext.getInstance("SSL");
+                sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+                // Create an ssl socket factory with our all-trusting manager
+                final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+                OkHttpClient.Builder builder = new OkHttpClient.Builder();
+                builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
+                builder.hostnameVerifier(new HostnameVerifier() {
+                    @Override
+                    public boolean verify(String hostname, SSLSession session) {
+                        // trust any host
+                        return true;
+                    }
+                });
+
+                return builder;
+            } catch (Exception e) {
+                throw new UnsupportedOperationException("Create unsafe OkHttpClient failed.", e);
+            }
         }
 
         private void release() {
