65b4e3054450a61cc56931f610537b220f7a54a5
jrfeng <jrfeng331@qq.com>
2020-09-02 11:53:17 +0800

    update debug code

--- debug/src/main/java/snow/player/debug/MyFactory.java
+++ debug/src/main/java/snow/player/debug/MyFactory.java
@@ -1,10 +1,9 @@
 package snow.player.debug;
 
 import android.content.Context;
-import android.util.Log;
+import android.net.Uri;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
@@ -12,11 +11,9 @@ import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
-import snow.player.HistoryRecorder;
 import snow.player.Inject;
 import snow.player.PlayerService;
 import snow.player.exo.ExoMusicPlayer;
-import snow.player.media.MusicItem;
 import snow.player.media.MusicPlayer;
 
 public class MyFactory extends PlayerService.ComponentFactory {
@@ -39,22 +36,9 @@ public class MyFactory extends PlayerService.ComponentFactory {
     }
 
     @Inject
-    @Nullable
-    @Override
-    public HistoryRecorder createHistoryRecorder() {
-        return new HistoryRecorder() {
-            @Override
-            public void recordHistory(@NonNull MusicItem musicItem) {
-                // DEBUG
-                Log.d("App", "History: " + musicItem.toString());
-            }
-        };
-    }
-
-    @Inject
     @NonNull
     @Override
-    public MusicPlayer createMusicPlayer(Context context) {
-        return new ExoMusicPlayer(context, mMediaSourceFactory);
+    public MusicPlayer createMusicPlayer(@NonNull Context context, @NonNull Uri uri) {
+        return new ExoMusicPlayer(context, mMediaSourceFactory, uri);
     }
 }
