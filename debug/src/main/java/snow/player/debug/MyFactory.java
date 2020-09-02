50ced13ce115ded53bf056f57287b63c4457ffc4
jrfeng <jrfeng331@qq.com>
2020-09-02 16:17:18 +0800

    update debug code

--- debug/src/main/java/snow/player/debug/MyFactory.java
+++ debug/src/main/java/snow/player/debug/MyFactory.java
@@ -13,19 +13,14 @@ import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import javax.net.ssl.SSLContext;
-
-import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
-import okhttp3.TlsVersion;
 import snow.player.Inject;
 import snow.player.PlayerService;
 import snow.player.exo.ExoMusicPlayer;
 import snow.player.media.MusicPlayer;
+import snow.player.util.OkHttpUtil;
 
 public class MyFactory extends PlayerService.ComponentFactory {
     private ProgressiveMediaSource.Factory mProgressiveMediaSourceFactory;
@@ -33,10 +28,18 @@ public class MyFactory extends PlayerService.ComponentFactory {
 
     @Override
     public void init(Context context) {
-        OkHttpClient client = getNewHttpClient();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .followRedirects(true)
+                .followSslRedirects(true)
+                .retryOnConnectionFailure(true)
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS);
+
+        OkHttpUtil.enableTls12OnPreLollipop(builder);
 
         OkHttpDataSourceFactory httpDataSourceFactory =
-                new OkHttpDataSourceFactory(client, Util.getUserAgent(context, context.getPackageName()));
+                new OkHttpDataSourceFactory(builder.build(), Util.getUserAgent(context, context.getPackageName()));
 
         DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(
                 context, httpDataSourceFactory);
@@ -56,43 +59,5 @@ public class MyFactory extends PlayerService.ComponentFactory {
         }
 
         return new ExoMusicPlayer(context, mProgressiveMediaSourceFactory, uri);
-    }
-
-    private OkHttpClient getNewHttpClient() {
-        OkHttpClient.Builder client = new OkHttpClient.Builder()
-                .followRedirects(true)
-                .followSslRedirects(true)
-                .retryOnConnectionFailure(true)
-                .cache(null)
-                .connectTimeout(10, TimeUnit.SECONDS)
-                .writeTimeout(20, TimeUnit.SECONDS)
-                .readTimeout(20, TimeUnit.SECONDS);
-
-        return enableTls12OnPreLollipop(client).build();
-    }
-
-    public static OkHttpClient.Builder enableTls12OnPreLollipop(OkHttpClient.Builder client) {
-        if (Build.VERSION.SDK_INT >= 16 && Build.VERSION.SDK_INT < 22) {
-            try {
-                SSLContext sc = SSLContext.getInstance("TLSv1.2");
-                sc.init(null, null, null);
-                client.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()));
-
-                ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-                        .tlsVersions(TlsVersion.TLS_1_2)
-                        .build();
-
-                List<ConnectionSpec> specs = new ArrayList<>();
-                specs.add(cs);
-                specs.add(ConnectionSpec.COMPATIBLE_TLS);
-                specs.add(ConnectionSpec.CLEARTEXT);
-
-                client.connectionSpecs(specs);
-            } catch (Exception exc) {
-                Log.e("OkHttpTLSCompat", "Error while setting TLS 1.2", exc);
-            }
-        }
-
-        return client;
     }
 }
