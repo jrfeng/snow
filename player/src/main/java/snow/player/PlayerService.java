7e5b0747d281aaedf51ff2419c9d54c234506b0d
jrfeng <jrfeng331@qq.com>
2020-09-24 13:36:57 +0800

    optimize 'PlayerService'

--- player/src/main/java/snow/player/PlayerService.java
+++ player/src/main/java/snow/player/PlayerService.java
@@ -500,6 +500,7 @@ public class PlayerService extends MediaBrowserServiceCompat
             getPlayer().pause();
         }
 
+        stopSelf();
         notifyOnShutdown();
         dismissKeepServiceAlive();
     }
