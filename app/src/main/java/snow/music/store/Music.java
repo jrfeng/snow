020167a7dbc40bbf39b8c9ec0b47298cd9940e2e
jrfeng <jrfeng331@qq.com>
2020-10-05 17:43:23 +0800

    create new types.
    new types:
        - Music
        - MusicList
        - ToManyWrapper

---
+++ app/src/main/java/snow/music/store/Music.java
@@ -0,0 +1,132 @@
+package snow.music.store;
+
+import androidx.annotation.NonNull;
+
+import com.google.common.base.Objects;
+
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+
+@Entity
+public class Music {
+    @Id
+    private long id;
+
+    private String title;
+    private String artist;
+    private String album;
+    private String uri;
+    private String iconUri;
+    private int duration;
+    private long addTime;
+
+    public Music(long id, String title, String artist, String album, String uri, String iconUri, int duration, long addTime) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.uri = uri;
+        this.iconUri = iconUri;
+        this.duration = duration;
+        this.addTime = addTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getIconUri() {
+        return iconUri;
+    }
+
+    public void setIconUri(String iconUri) {
+        this.iconUri = iconUri;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public long getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(long addTime) {
+        this.addTime = addTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Music music = (Music) o;
+        return id == music.id &&
+                duration == music.duration &&
+                addTime == music.addTime &&
+                Objects.equal(title, music.title) &&
+                Objects.equal(artist, music.artist) &&
+                Objects.equal(album, music.album) &&
+                Objects.equal(uri, music.uri) &&
+                Objects.equal(iconUri, music.iconUri);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, title, artist, album, uri, iconUri, duration, addTime);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Music{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", album='" + album + '\'' +
+                ", uri='" + uri + '\'' +
+                ", iconUri='" + iconUri + '\'' +
+                ", duration=" + duration +
+                ", addTime=" + addTime +
+                '}';
+    }
+}
---
+++ app/src/main/java/snow/music/store/MusicList.java
@@ -0,0 +1,101 @@
+package snow.music.store;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.common.base.Objects;
+
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+import io.objectbox.annotation.Unique;
+import io.objectbox.relation.ToMany;
+import snow.music.util.ToManyWrapper;
+
+@Entity
+public class MusicList implements ToManyWrapper.OrderedSource<Music> {
+    @Id
+    private long id;
+    @Unique
+    private String name;
+    private String description;
+    public byte[] orderBytes;
+
+    public ToMany<Music> musicElements;
+
+    public MusicList(long id, String name, String description, byte[] orderBytes) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.orderBytes = orderBytes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MusicList musicList1 = (MusicList) o;
+        return id == musicList1.id &&
+                Objects.equal(name, musicList1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, name);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "MusicList{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @NonNull
+    @Override
+    public ToMany<Music> getToMany() {
+        return this.musicElements;
+    }
+
+    @Nullable
+    @Override
+    public byte[] getOrderBytes() {
+        return this.orderBytes;
+    }
+
+    @Override
+    public void setOrderBytes(byte[] orderBytes) {
+        this.orderBytes = orderBytes;
+    }
+
+    @Override
+    public long getTargetId(Music music) {
+        return music.getId();
+    }
+}
