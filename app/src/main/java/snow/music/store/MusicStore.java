dbea36bcf30dedf504d3eccf1a18bb6b3f73dc99
jrfeng <jrfeng331@qq.com>
2020-10-06 15:25:51 +0800

    optimize 'MusicStore'

--- app/src/main/java/snow/music/store/MusicStore.java
+++ app/src/main/java/snow/music/store/MusicStore.java
@@ -8,25 +8,35 @@ import androidx.annotation.Nullable;
 import com.google.common.base.Preconditions;
 
 import java.io.File;
+import java.util.List;
 
 import io.objectbox.Box;
 import io.objectbox.BoxStore;
 
 public class MusicStore {
-    private static final String MUSIC_LIST_LOCAL_MUSIC = "__local_music";
-    private static final String MUSIC_LIST_FAVORITE = "__favorite";
-    private static final String MUSIC_LIST_HISTORY = "__history";
+    public static final String MUSIC_LIST_LOCAL_MUSIC = "__local_music";
+    public static final String MUSIC_LIST_FAVORITE = "__favorite";
+    public static final String MUSIC_LIST_HISTORY = "__history";
+
+    private static final int MAX_HISTORY_SIZE = 500;
 
     private static MusicStore mInstance;
 
     private Box<Music> mMusicBox;
     private Box<MusicListEntity> mMusicListEntityBox;
 
+    private MusicList mHistory;
+
     private MusicStore(BoxStore boxStore) {
         mMusicBox = boxStore.boxFor(Music.class);
         mMusicListEntityBox = boxStore.boxFor(MusicListEntity.class);
     }
 
+    /**
+     * 初始化 {@link MusicStore}
+     *
+     * @param context Context 对象，不能为 null
+     */
     public synchronized static void init(@NonNull Context context) {
         Preconditions.checkNotNull(context);
 
@@ -41,6 +51,11 @@ public class MusicStore {
         mInstance = new MusicStore(boxStore);
     }
 
+    /**
+     * 初始化 {@link MusicStore}
+     *
+     * @param boxStore BoxStore 对象，不能为 null
+     */
     public synchronized static void init(@NonNull BoxStore boxStore) {
         Preconditions.checkNotNull(boxStore);
 
@@ -62,7 +77,7 @@ public class MusicStore {
     /**
      * 歌单是否已存在。
      */
-    public boolean isMusicListExists(@NonNull String name) {
+    public synchronized boolean isMusicListExists(@NonNull String name) {
         Preconditions.checkNotNull(name);
 
         long count = mMusicListEntityBox.query()
@@ -79,12 +94,12 @@ public class MusicStore {
      * @throws IllegalArgumentException 如果 name 参数是个空字符串，则抛出该异常。
      */
     @NonNull
-    public MusicList createMusicList(@NonNull String name, @NonNull String description) throws IllegalArgumentException {
+    public synchronized MusicList createMusicList(@NonNull String name, @NonNull String description) throws IllegalArgumentException {
         Preconditions.checkNotNull(name);
         Preconditions.checkNotNull(description);
         Preconditions.checkArgument(name.isEmpty(), "name must not empty");
 
-        if (isBuiltIn(name)) {
+        if (isBuiltInName(name)) {
             throw new IllegalArgumentException("Illegal music list name, conflicts with built-in name.");
         }
 
@@ -103,10 +118,10 @@ public class MusicStore {
      * 获取自建歌单，如果自建歌单不存在，则返回 null。
      */
     @Nullable
-    public MusicList getMusicList(@NonNull String name) {
+    public synchronized MusicList getMusicList(@NonNull String name) {
         Preconditions.checkNotNull(name);
 
-        if (isBuiltIn(name)) {
+        if (isBuiltInName(name)) {
             return null;
         }
 
@@ -125,12 +140,8 @@ public class MusicStore {
     /**
      * 更新歌单。
      */
-    public void updateMusicList(@NonNull MusicList musicList) {
+    public synchronized void updateMusicList(@NonNull MusicList musicList) {
         Preconditions.checkNotNull(musicList);
-
-        if (isBuiltIn(musicList.getName())) {
-            return;
-        }
 
         if (!isMusicListExists(musicList.getName())) {
             return;
@@ -142,19 +153,23 @@ public class MusicStore {
 
     /**
      * 删除歌单。
+     * <p>
+     * 不允许删除内置歌单。
      */
-    public void deleteMusicList(@NonNull MusicList musicList) {
+    public synchronized void deleteMusicList(@NonNull MusicList musicList) {
         Preconditions.checkNotNull(musicList);
         deleteMusicList(musicList.getName());
     }
 
     /**
      * 删除歌单。
+     * <p>
+     * 不允许删除内置歌单。
      */
-    public void deleteMusicList(@NonNull String name) {
+    public synchronized void deleteMusicList(@NonNull String name) {
         Preconditions.checkNotNull(name);
 
-        if (isBuiltIn(name)) {
+        if (isBuiltInName(name)) {
             return;
         }
 
@@ -164,13 +179,19 @@ public class MusicStore {
                 .remove();
     }
 
-    public boolean isFavorite(@NonNull Music music) {
+    /**
+     * 歌曲是否是 “我喜欢”
+     */
+    public synchronized boolean isFavorite(@NonNull Music music) {
         Preconditions.checkNotNull(music);
 
         return isFavorite(music.getId());
     }
 
-    public boolean isFavorite(long musicId) {
+    /**
+     * 指定 musicId 的歌曲是否是 “我喜欢”
+     */
+    public synchronized boolean isFavorite(long musicId) {
         if (musicId <= 0) {
             return false;
         }
@@ -185,10 +206,87 @@ public class MusicStore {
         return count > 0;
     }
 
-    private boolean isBuiltIn(String name) {
+    /**
+     * 获取 “本地音乐” 歌单。
+     */
+    public synchronized MusicList getLocalMusicList() {
+        return getBuiltInMusicList(MUSIC_LIST_LOCAL_MUSIC);
+    }
+
+    /**
+     * 获取 “我喜欢” 歌单。
+     */
+    public synchronized MusicList getFavoriteMusicList() {
+        return getBuiltInMusicList(MUSIC_LIST_FAVORITE);
+    }
+
+    /**
+     * 指定 name 名称是否是内置歌单名。如果是，则返回 true，否则返回 false。
+     */
+    public static boolean isBuiltInName(String name) {
         return name.equalsIgnoreCase(MUSIC_LIST_LOCAL_MUSIC) ||
                 name.equalsIgnoreCase(MUSIC_LIST_FAVORITE) ||
                 name.equalsIgnoreCase(MUSIC_LIST_HISTORY);
+    }
+
+    /**
+     * 添加一条历史记录。
+     */
+    public synchronized void addHistory(@NonNull Music music) {
+        Preconditions.checkNotNull(music);
+
+        MusicList history = getHistoryMusicList();
+        List<Music> elements = history.getMusicElements();
+
+        elements.remove(music);
+        elements.add(music);
+
+        if (history.getSize() - MAX_HISTORY_SIZE > 0) {
+            elements.remove(0);
+        }
+
+        updateMusicList(history);
+    }
+
+    /**
+     * 移除一条历史记录。
+     */
+    public synchronized void removeHistory(@NonNull Music music) {
+        Preconditions.checkNotNull(music);
+
+        MusicList history = getHistoryMusicList();
+        history.getMusicElements().remove(music);
+
+        updateMusicList(history);
+    }
+
+    /**
+     * 移除多条历史记录。
+     */
+    public synchronized void removeHistory(@NonNull List<Music> musics) {
+        Preconditions.checkNotNull(musics);
+
+        MusicList history = getHistoryMusicList();
+        history.getMusicElements().removeAll(musics);
+
+        updateMusicList(history);
+    }
+
+    /**
+     * 清空历史记录。
+     */
+    public synchronized void clearHistory() {
+        MusicList history = getHistoryMusicList();
+        history.getMusicElements().clear();
+        updateMusicList(history);
+    }
+
+    private synchronized MusicList getHistoryMusicList() {
+        if (mHistory == null) {
+            mHistory = getBuiltInMusicList(MUSIC_LIST_HISTORY);
+        }
+
+        return mHistory;
     }
 
     @NonNull
