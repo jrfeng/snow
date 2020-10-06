f3cb84a5ad6af410c1661d7ac5db268e83c0a628
jrfeng <jrfeng331@qq.com>
2020-10-06 18:41:26 +0800

    update 'MusicListTest'

--- app/src/test/java/snow/music/store/MusicListTest.java
+++ app/src/test/java/snow/music/store/MusicListTest.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import io.objectbox.BoxStore;
 
@@ -18,7 +20,7 @@ public class MusicListTest {
     private MusicStore mMusicStore;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         BoxStore.deleteAllFiles(TEST_DIRECTORY);
         store = MyObjectBox.builder()
                 .directory(TEST_DIRECTORY)
@@ -28,7 +30,7 @@ public class MusicListTest {
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
         if (store != null) {
             store.close();
             store = null;
@@ -126,12 +128,392 @@ public class MusicListTest {
         musicList.getMusicElements().add(musicB);
         musicList.getMusicElements().add(musicB);
 
-        mMusicStore.updateMusicList(musicList);
-
         assertEquals(2, musicList.getSize());
         assertEquals(musicA, musicList.getMusicElements().get(0));
         assertEquals(musicB, musicList.getMusicElements().get(1));
     }
 
+    @Test
+    public void elements_add_index() {
+        final Music musicA = new Music(
+                0,
+                "title1",
+                "artist1",
+                "album1",
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
 
+        final Music musicB = new Music(
+                0,
+                "title2",
+                "artist2",
+                "album2",
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        MusicList musicList = mMusicStore.createMusicList(TEST_MUSIC_LIST);
+
+        musicList.getMusicElements().add(musicA);
+        musicList.getMusicElements().add(musicB);
+
+        musicList.getMusicElements().add(1, musicB);
+
+        assertEquals(2, musicList.getSize());
+        assertEquals(musicA, musicList.getMusicElements().get(0));
+        assertEquals(musicB, musicList.getMusicElements().get(1));
+
+        musicList.getMusicElements().add(0, musicB);
+        assertEquals(musicB, musicList.getMusicElements().get(0));
+        assertEquals(musicA, musicList.getMusicElements().get(1));
+    }
+
+    @Test
+    public void elements_addAll() {
+        final Music musicA = new Music(
+                0,
+                "title1",
+                "artist1",
+                "album1",
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                "artist2",
+                "album2",
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicC = new Music(
+                0,
+                "title3",
+                "artist3",
+                "album3",
+                "https://www.test.com/test3.mp3",
+                "https://www.test.com/test3.png",
+                60_000,
+                System.currentTimeMillis());
+
+        MusicList musicList = mMusicStore.createMusicList(TEST_MUSIC_LIST);
+        musicList.getMusicElements().add(musicA);
+
+        List<Music> musics = new ArrayList<>();
+        musics.add(musicB);
+        musics.add(musicA);
+        musics.add(musicC);
+        musics.add(musicA);
+
+        musicList.getMusicElements().addAll(musics);
+
+        assertEquals(3, musicList.getSize());
+        assertEquals(musicA, musicList.getMusicElements().get(0));
+        assertEquals(musicB, musicList.getMusicElements().get(1));
+        assertEquals(musicC, musicList.getMusicElements().get(2));
+    }
+
+    @Test
+    public void elements_addAll_index() {
+        final Music musicA = new Music(
+                0,
+                "title1",
+                "artist1",
+                "album1",
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                "artist2",
+                "album2",
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicC = new Music(
+                0,
+                "title3",
+                "artist3",
+                "album3",
+                "https://www.test.com/test3.mp3",
+                "https://www.test.com/test3.png",
+                60_000,
+                System.currentTimeMillis());
+
+        MusicList musicList = mMusicStore.createMusicList(TEST_MUSIC_LIST);
+        musicList.getMusicElements().add(musicA);
+
+        List<Music> musics = new ArrayList<>();
+        musics.add(musicB);
+        musics.add(musicA);
+        musics.add(musicC);
+        musics.add(musicB);
+
+        musicList.getMusicElements().addAll(0, musics);
+
+        assertEquals(3, musicList.getSize());
+        assertEquals(musicB, musicList.getMusicElements().get(0));
+        assertEquals(musicC, musicList.getMusicElements().get(1));
+        assertEquals(musicA, musicList.getMusicElements().get(2));
+    }
+
+    @Test
+    public void elements_remove() {
+        final Music musicA = new Music(
+                0,
+                "title1",
+                "artist1",
+                "album1",
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                "artist2",
+                "album2",
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        MusicList musicList = mMusicStore.createMusicList(TEST_MUSIC_LIST);
+        musicList.getMusicElements().add(musicA);
+        musicList.getMusicElements().add(musicB);
+
+        musicList.getMusicElements().remove(musicA);
+
+        assertEquals(1, musicList.getSize());
+        assertEquals(musicB, musicList.getMusicElements().get(0));
+    }
+
+    @Test
+    public void elements_remove_index() {
+        final Music musicA = new Music(
+                0,
+                "title1",
+                "artist1",
+                "album1",
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                "artist2",
+                "album2",
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        MusicList musicList = mMusicStore.createMusicList(TEST_MUSIC_LIST);
+        musicList.getMusicElements().add(musicA);
+        musicList.getMusicElements().add(musicB);
+
+        musicList.getMusicElements().remove(0);
+
+        assertEquals(1, musicList.getSize());
+        assertEquals(musicB, musicList.getMusicElements().get(0));
+    }
+
+    @Test
+    public void elements_removeAll() {
+        final Music musicA = new Music(
+                0,
+                "title1",
+                "artist1",
+                "album1",
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                "artist2",
+                "album2",
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        MusicList musicList = mMusicStore.createMusicList(TEST_MUSIC_LIST);
+        musicList.getMusicElements().add(musicA);
+        musicList.getMusicElements().add(musicB);
+
+        List<Music> musics = new ArrayList<>();
+        musics.add(musicA);
+        musics.add(musicA);
+        musics.add(musicB);
+        musics.add(musicB);
+
+        musicList.getMusicElements().removeAll(musics);
+
+        assertEquals(0, musicList.getSize());
+    }
+
+    @Test
+    public void elements_set() {
+        final Music musicA = new Music(
+                0,
+                "title1",
+                "artist1",
+                "album1",
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                "artist2",
+                "album2",
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicC = new Music(
+                0,
+                "title3",
+                "artist3",
+                "album3",
+                "https://www.test.com/test3.mp3",
+                "https://www.test.com/test3.png",
+                60_000,
+                System.currentTimeMillis());
+
+        MusicList musicList = mMusicStore.createMusicList(TEST_MUSIC_LIST);
+        musicList.getMusicElements().add(musicA);
+        musicList.getMusicElements().add(musicB);
+
+        musicList.getMusicElements().set(1, musicC);
+
+        assertEquals(musicC, musicList.getMusicElements().get(1));
+
+        musicList.getMusicElements().add(musicB);
+        musicList.getMusicElements().set(2, musicC);
+
+        assertEquals(2, musicList.getSize());
+        assertEquals(musicA, musicList.getMusicElements().get(0));
+        assertEquals(musicC, musicList.getMusicElements().get(1));
+    }
+
+    @Test
+    public void elements_retainAll() {
+        final Music musicA = new Music(
+                0,
+                "title1",
+                "artist1",
+                "album1",
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                "artist2",
+                "album2",
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicC = new Music(
+                0,
+                "title3",
+                "artist3",
+                "album3",
+                "https://www.test.com/test3.mp3",
+                "https://www.test.com/test3.png",
+                60_000,
+                System.currentTimeMillis());
+
+        MusicList musicList = mMusicStore.createMusicList(TEST_MUSIC_LIST);
+        musicList.getMusicElements().add(musicA);
+        musicList.getMusicElements().add(musicB);
+
+        List<Music> retain = new ArrayList<>();
+        retain.add(musicB);
+        retain.add(musicC);
+
+        musicList.getMusicElements().retainAll(retain);
+
+        assertEquals(1, musicList.getSize());
+        assertEquals(musicB, musicList.getMusicElements().get(0));
+    }
+
+    @Test
+    public void elements_orders() {
+        final Music musicA = new Music(
+                0,
+                "title1",
+                "artist1",
+                "album1",
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                "artist2",
+                "album2",
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicC = new Music(
+                0,
+                "title3",
+                "artist3",
+                "album3",
+                "https://www.test.com/test3.mp3",
+                "https://www.test.com/test3.png",
+                60_000,
+                System.currentTimeMillis());
+
+        List<Music> musics = new ArrayList<>();
+        musics.add(musicA);
+        musics.add(musicB);
+        musics.add(musicC);
+
+        mMusicStore.putAllMusic(musics);
+
+        MusicList musicList = mMusicStore.createMusicList(TEST_MUSIC_LIST);
+        musicList.getMusicElements().add(musicB);
+        musicList.getMusicElements().add(musicC);
+        musicList.getMusicElements().add(musicA);
+
+        mMusicStore.updateMusicList(musicList);
+
+        musicList = mMusicStore.createMusicList(TEST_MUSIC_LIST);
+
+        assertEquals(musicB, musicList.getMusicElements().get(0));
+        assertEquals(musicC, musicList.getMusicElements().get(1));
+        assertEquals(musicA, musicList.getMusicElements().get(2));
+    }
 }
