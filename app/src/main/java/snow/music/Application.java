814b2c30574bb3ea8d5562a0e0ca5f983e35b525
jrfeng <jrfeng331@qq.com>
2020-10-12 11:07:39 +0800

    optimize WelcomeActivity

--- app/src/main/java/snow/music/Application.java
+++ app/src/main/java/snow/music/Application.java
@@ -2,15 +2,18 @@ package snow.music;
 
 import androidx.multidex.MultiDexApplication;
 
-import java.io.File;
+import com.tencent.mmkv.MMKV;
 
 import snow.music.store.MusicStore;
+import snow.music.util.NightModeUtil;
 
 public class Application extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
 
+        MMKV.initialize(this);
+        NightModeUtil.applyNightMode(this);
         MusicStore.init(this);
     }
 }
--- app/src/main/java/snow/music/WelcomeActivity.java
+++ app/src/main/java/snow/music/WelcomeActivity.java
@@ -2,13 +2,42 @@ package snow.music;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+
 public class WelcomeActivity extends AppCompatActivity {
+    private Disposable mTimerDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startTimer();
+    }
+
+    public void startTimer() {
+        if (mTimerDisposable != null && !mTimerDisposable.isDisposed()) {
+            mTimerDisposable.isDisposed();
+        }
+
+        mTimerDisposable = Observable.timer(1500, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::startMainActivity);
+    }
+
+    public void startMainActivity(Long along) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
