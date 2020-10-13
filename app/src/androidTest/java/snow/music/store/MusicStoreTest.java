810b49dc6766ea4a6633e8764a316be2acd24adc
jrfeng <jrfeng331@qq.com>
2020-10-13 12:10:32 +0800

    update MusicStoreTest

--- app/src/androidTest/java/snow/music/store/MusicStoreTest.java
+++ app/src/androidTest/java/snow/music/store/MusicStoreTest.java
@@ -1,8 +1,14 @@
 package snow.music.store;
 
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -12,16 +18,23 @@ import io.objectbox.BoxStore;
 
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
 public class MusicStoreTest {
-    private static final File TEST_DIRECTORY = new File("objectbox-debug/test-db");
+    private File test_directory;
     private BoxStore store;
     private MusicStore mMusicStore;
 
+    private Context getContext() {
+        return InstrumentationRegistry.getInstrumentation().getTargetContext();
+    }
+
     @Before
     public void setUp() {
-        BoxStore.deleteAllFiles(TEST_DIRECTORY);
+        test_directory = new File(getContext().getCacheDir(), "objectbox-test");
+
+        BoxStore.deleteAllFiles(test_directory);
         store = MyObjectBox.builder()
-                .directory(TEST_DIRECTORY)
+                .directory(test_directory)
                 .build();
         MusicStore.init(store);
         mMusicStore = MusicStore.getInstance();
@@ -33,7 +46,7 @@ public class MusicStoreTest {
             store.close();
             store = null;
         }
-        BoxStore.deleteAllFiles(TEST_DIRECTORY);
+        BoxStore.deleteAllFiles(test_directory);
     }
 
     @Test
@@ -379,5 +392,197 @@ public class MusicStoreTest {
         assertEquals(2, allMusicList.size());
         assertEquals(nameA, allMusicList.get(0).getName());
         assertEquals(nameB, allMusicList.get(1).getName());
+    }
+
+    @Test
+    public void getAllArtist() {
+        assertNotNull(mMusicStore.getAllArtist());
+
+        final String artistA = "Artist A";
+        final String artistB = "Artist B";
+
+        final Music musicA = new Music(
+                0,
+                "title1",
+                artistA,
+                "album1",
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                artistA,
+                "album2",
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicC = new Music(
+                0,
+                "title3",
+                artistB,
+                "album3",
+                "https://www.test.com/test3.mp3",
+                "https://www.test.com/test3.png",
+                60_000,
+                System.currentTimeMillis());
+
+        mMusicStore.putMusic(musicA);
+        mMusicStore.putMusic(musicB);
+        mMusicStore.putMusic(musicC);
+
+        List<String> allArtist = mMusicStore.getAllArtist();
+
+        assertEquals(2, allArtist.size());
+        assertTrue(allArtist.contains(artistA));
+        assertTrue(allArtist.contains(artistB));
+    }
+
+    @Test
+    public void getAllAlbum() {
+        assertNotNull(mMusicStore.getAllAlbum());
+
+        final String albumA = "Album A";
+        final String albumB = "Album B";
+
+        final Music musicA = new Music(
+                0,
+                "title1",
+                "artist1",
+                albumA,
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                "artist2",
+                albumA,
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicC = new Music(
+                0,
+                "title3",
+                "artist3",
+                albumB,
+                "https://www.test.com/test3.mp3",
+                "https://www.test.com/test3.png",
+                60_000,
+                System.currentTimeMillis());
+
+        mMusicStore.putMusic(musicA);
+        mMusicStore.putMusic(musicB);
+        mMusicStore.putMusic(musicC);
+
+        List<String> allAlbum = mMusicStore.getAllAlbum();
+
+        assertEquals(2, allAlbum.size());
+        assertTrue(allAlbum.contains(albumA));
+        assertTrue(allAlbum.contains(albumB));
+    }
+
+    @Test
+    public void getArtistAllMusic() {
+        assertNotNull(mMusicStore.getArtistAllMusic("none"));
+
+        final String artistA = "Artist A";
+        final String artistB = "Artist B";
+
+        final Music musicA = new Music(
+                0,
+                "title1",
+                artistA,
+                "album1",
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                artistA,
+                "album2",
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicC = new Music(
+                0,
+                "title3",
+                artistB,
+                "album3",
+                "https://www.test.com/test3.mp3",
+                "https://www.test.com/test3.png",
+                60_000,
+                System.currentTimeMillis());
+
+        mMusicStore.putMusic(musicA);
+        mMusicStore.putMusic(musicB);
+        mMusicStore.putMusic(musicC);
+
+        List<Music> musicsA = mMusicStore.getArtistAllMusic(artistA);
+        List<Music> musicsB = mMusicStore.getArtistAllMusic(artistB);
+
+        assertEquals(2, musicsA.size());
+        assertEquals(1, musicsB.size());
+    }
+
+    @Test
+    public void getAlbumAllMusic() {
+        assertNotNull(mMusicStore.getAlbumAllMusic("none"));
+
+        final String albumA = "Album A";
+        final String albumB = "Album B";
+
+        final Music musicA = new Music(
+                0,
+                "title1",
+                "artist1",
+                albumA,
+                "https://www.test.com/test1.mp3",
+                "https://www.test.com/test1.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicB = new Music(
+                0,
+                "title2",
+                "artist2",
+                albumA,
+                "https://www.test.com/test2.mp3",
+                "https://www.test.com/test2.png",
+                60_000,
+                System.currentTimeMillis());
+
+        final Music musicC = new Music(
+                0,
+                "title3",
+                "artist3",
+                albumB,
+                "https://www.test.com/test3.mp3",
+                "https://www.test.com/test3.png",
+                60_000,
+                System.currentTimeMillis());
+
+        mMusicStore.putMusic(musicA);
+        mMusicStore.putMusic(musicB);
+        mMusicStore.putMusic(musicC);
+
+        List<Music> musicsA = mMusicStore.getAlbumAllMusic(albumA);
+        List<Music> musicsB = mMusicStore.getAlbumAllMusic(albumB);
+
+        assertEquals(2, musicsA.size());
+        assertEquals(1, musicsB.size());
     }
 }
