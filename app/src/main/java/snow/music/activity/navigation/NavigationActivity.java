1c0722f8cf058acc8a7f08ed06df068f1d03b0d8
jrfeng <jrfeng331@qq.com>
2020-10-23 16:07:20 +0800

    request external storage permission & scan local music

--- app/src/main/java/snow/music/activity/navigation/NavigationActivity.java
+++ app/src/main/java/snow/music/activity/navigation/NavigationActivity.java
@@ -1,12 +1,18 @@
 package snow.music.activity.navigation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.PagerSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import snow.music.R;
@@ -16,6 +22,11 @@ import snow.player.PlayerService;
 import snow.player.lifecycle.PlayerViewModel;
 
 public class NavigationActivity extends AppCompatActivity {
+    private static final String KEY_SCAN_LOCAL_MUSIC = "scan_local_music";
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private boolean mScanOnPermissionGranted;
+    private boolean mRepeatedRequestStoragePermission;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +45,10 @@ public class NavigationActivity extends AppCompatActivity {
 
         binding.setPlayerViewModel(playerViewModel);
         binding.setNavViewModel(navigationViewModel);
+
+        if (shouldScanLocalMusic()) {
+            scanLocalMusic();
+        }
     }
 
     private void initDiskPanel(RecyclerView diskPanel, PlayerViewModel playerViewModel) {
@@ -55,5 +70,68 @@ public class NavigationActivity extends AppCompatActivity {
         playerClient.connect();
 
         playerViewModel.setAutoDisconnect(true);
+    }
+
+    private boolean shouldScanLocalMusic() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        return preferences.getBoolean(KEY_SCAN_LOCAL_MUSIC, true);
+    }
+
+    private void scanLocalMusic() {
+        if (hasStoragePermission()) {
+            scanLocalMusicAsync();
+            return;
+        }
+
+        mScanOnPermissionGranted = true;
+        requestStoragePermission();
+    }
+
+    private boolean hasStoragePermission() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestStoragePermission() {
+        if (hasStoragePermission()) {
+            return;
+        }
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode != PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (grantResults.length <= 0 || grantResults[0] == PackageManager.PERMISSION_DENIED) {
+            showRequestPermissionRationale();
+            return;
+        }
+
+        if (mScanOnPermissionGranted) {
+            scanLocalMusicAsync();
+        }
+    }
+
+    private void showRequestPermissionRationale() {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            return;
+        }
+
+        if (mRepeatedRequestStoragePermission) {
+            return;
+        }
+
+        mRepeatedRequestStoragePermission = true;
+        requestStoragePermission();
+    }
+
+    private void scanLocalMusicAsync() {
+        // TODO
     }
 }
