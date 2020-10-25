af3ace3147077331a7cd0f343cc96f2260db57bc
jrfeng <jrfeng331@qq.com>
2020-10-25 09:35:45 +0800

    delete 'MyAppGlideModule.java'

--- app/src/main/java/snow/music/MyAppGlideModule.java
+++
@@ -1,8 +0,0 @@
-package snow.music;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public class MyAppGlideModule extends AppGlideModule {
-}
