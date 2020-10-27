9fcb71396d7449f9f86bb61216d0191ab99f2617
jrfeng <jrfeng331@qq.com>
2020-10-27 19:08:55 +0800

    add docs

--- app/src/main/java/snow/music/util/DialogUtil.java
+++ app/src/main/java/snow/music/util/DialogUtil.java
@@ -21,6 +21,12 @@ public final class DialogUtil {
         throw new AssertionError();
     }
 
+    /**
+     * 设置 Dialog 内容的 Gravity
+     *
+     * @param dialog  Dialog 对象，不能为 null
+     * @param gravity Gravity 常量，具体请参考 {@code android.view.Gravity} 类
+     */
     public static void setGravity(@NonNull Dialog dialog, int gravity) {
         Preconditions.checkNotNull(dialog);
 
@@ -33,6 +39,12 @@ public final class DialogUtil {
         window.setGravity(gravity);
     }
 
+    /**
+     * 设置 Dialog 内容的宽度。
+     *
+     * @param dialog Dialog 对象，不能为 null
+     * @param width  宽度值
+     */
     public static void setWith(@NonNull Dialog dialog, int width) {
         Preconditions.checkNotNull(dialog);
 
@@ -50,6 +62,12 @@ public final class DialogUtil {
         window.setAttributes(lp);
     }
 
+    /**
+     * 设置 Dialog 内容的高度。
+     *
+     * @param dialog Dialog 对象，不能为 null
+     * @param height 高度值
+     */
     public static void setHeight(@NonNull Dialog dialog, int height) {
         Preconditions.checkNotNull(dialog);
 
@@ -67,6 +85,52 @@ public final class DialogUtil {
         window.setAttributes(lp);
     }
 
+    /**
+     * 设置 Dialog 的进入/退出动画。
+     * <p>
+     * 进入退出动画的一个 style 资源，例如：
+     * <p>
+     * <pre>
+     * {@code
+     * <style name="OptionMenuAnim">
+     *     <item name="android:windowEnterAnimation">@anim/enter_from_bottom</item>
+     *     <item name="android:windowExitAnimation">@anim/exit_from_bottom</item>
+     * </style>
+     * }
+     * </pre>
+     * <p>
+     * 其中，{@code android:windowEnterAnimation} 与 {@code android:windowExitAnimation} 是两个动画资源，
+     * 例如：<p>
+     * 动画资源：{@code @anim/enter_from_bottom}<br>
+     * <pre>
+     * {@code
+     * <?xml version="1.0" encoding="utf-8"?>
+     * <set xmlns:android="http://schemas.android.com/apk/res/android">
+     *     <translate
+     *         android:duration="200"
+     *         android:fromYDelta="100%"
+     *         android:interpolator="@android:anim/accelerate_interpolator"
+     *         android:toYDelta="0%"/>
+     * </set>
+     * }
+     * </pre>
+     * 动画资源：{@code @anim/exit_from_bottom}<br>
+     * <pre>
+     * {@code
+     * <?xml version="1.0" encoding="utf-8"?>
+     * <set xmlns:android="http://schemas.android.com/apk/res/android">
+     *     <translate
+     *         android:duration="100"
+     *         android:fromYDelta="0%"
+     *         android:interpolator="@android:anim/decelerate_interpolator"
+     *         android:toYDelta="100%"/>
+     * </set>
+     * }
+     * </pre>
+     *
+     * @param dialog   Dialog 对象，不能为 null
+     * @param styleRes style 资源
+     */
     public static void setAnimations(@NonNull Dialog dialog, @StyleRes int styleRes) {
         Preconditions.checkNotNull(dialog);
 
