225c5db37d387d0f16d0fd01916de83317ad65c5
jrfeng <jrfeng331@qq.com>
2020-10-29 12:15:45 +0800

    optimize PlaylistDialog

--- app/src/main/java/snow/music/dialog/PlaylistDialog.java
+++ app/src/main/java/snow/music/dialog/PlaylistDialog.java
@@ -1,37 +1,51 @@
 package snow.music.dialog;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.common.base.Preconditions;
 
+import recyclerview.helper.ItemClickHelper;
+import recyclerview.helper.ScrollToPositionHelper;
+import recyclerview.helper.SelectableHelper;
 import snow.music.R;
 import snow.music.util.DialogUtil;
 import snow.player.PlayerClient;
-import snow.player.PlayerService;
 import snow.player.audio.MusicItem;
 import snow.player.lifecycle.PlayerViewModel;
+import snow.player.lifecycle.PlaylistLiveData;
 import snow.player.playlist.Playlist;
 
 public class PlaylistDialog extends AppCompatDialogFragment {
-    private Context mContext;
     private PlayerViewModel mPlayerViewModel;
     private PlaylistAdapter mPlaylistAdapter;
+    private PlaylistLiveData mPlaylistLiveData;
+
+    private TextView tvPlaylistTitle;
+    private RecyclerView rvPlaylist;
+
+    private ScrollToPositionHelper mScrollToPositionHelper;
 
     public static PlaylistDialog newInstance() {
         return new PlaylistDialog();
@@ -40,22 +54,12 @@ public class PlaylistDialog extends AppCompatDialogFragment {
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        mContext = context;
 
-        ViewModelProvider provider = new ViewModelProvider(this);
+        FragmentActivity ownerActivity = getActivity();
+        assert ownerActivity != null;
+
+        ViewModelProvider provider = new ViewModelProvider(ownerActivity);
         mPlayerViewModel = provider.get(PlayerViewModel.class);
-        initPlayerViewModel();
-        initPlaylistAdapter();
-
-        mPlayerViewModel.getPlaylist()
-                .observe(this, playlist -> {
-                    mPlaylistAdapter.setPlaylist(playlist);
-                });
-
-        mPlayerViewModel.getPlayPosition()
-                .observe(this, playPosition -> {
-                    mPlaylistAdapter.setPlayPosition(playPosition);
-                });
     }
 
     @NonNull
@@ -70,61 +74,126 @@ public class PlaylistDialog extends AppCompatDialogFragment {
         dialog.setCanceledOnTouchOutside(true);
 
         dialog.setContentView(R.layout.dialog_playlist);
-        RecyclerView rvPlaylist = dialog.findViewById(R.id.rvPlaylist);
-        if (rvPlaylist == null) {
-            throw new IllegalStateException("RecyclerView is null.");
-        }
 
-        rvPlaylist.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false));
-        rvPlaylist.setAdapter(mPlaylistAdapter);
+        tvPlaylistTitle = dialog.findViewById(R.id.tvPlaylistTitle);
+        rvPlaylist = dialog.findViewById(R.id.rvPlaylist);
+
+        initRecyclerView(rvPlaylist);
+        mScrollToPositionHelper = new ScrollToPositionHelper(rvPlaylist, getResources().getColor(R.color.deep_purple_50));
+
+        ImageButton btnLocate = dialog.findViewById(R.id.btnLocate);
+        assert btnLocate != null;
+        btnLocate.setOnClickListener(v -> mScrollToPositionHelper.smoothScrollToPosition(mPlayerViewModel.getPlayerClient().getPlayPosition()));
 
         return dialog;
     }
 
-    private void initPlayerViewModel() {
-        if (mPlayerViewModel.isInitialized()) {
-            return;
+    private void initRecyclerView(RecyclerView rvPlaylist) {
+        Context context = getContext();
+        if (context == null) {
+            throw new IllegalStateException("Context is null.");
         }
 
-        PlayerClient playerClient = PlayerClient.newInstance(mContext, PlayerService.class);
-        playerClient.setAutoConnect(true);
-        playerClient.connect();
-
-        mPlayerViewModel.init(mContext, playerClient);
-        mPlayerViewModel.setAutoDisconnect(true);
+        rvPlaylist.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
+        initPlaylistAdapter();
     }
 
+    @SuppressLint("SetTextI18n")
     private void initPlaylistAdapter() {
-        Playlist playlist = mPlayerViewModel.getPlaylist().getValue();
-        Integer playPosition = mPlayerViewModel.getPlayPosition().getValue();
-        assert playlist != null;
-        assert playPosition != null;
+        PlayerClient playerClient = mPlayerViewModel.getPlayerClient();
+        playerClient.getPlaylist(playlist -> {
+            mPlaylistLiveData = new PlaylistLiveData(playerClient, playlist);
+            mPlaylistAdapter = new PlaylistAdapter(playlist, playerClient.getPlayPosition());
+            rvPlaylist.setAdapter(mPlaylistAdapter);
+            rvPlaylist.scrollToPosition(playerClient.getPlayPosition());
 
-        mPlaylistAdapter = new PlaylistAdapter(playlist, playPosition);
+            mPlaylistAdapter.setOnItemClickListener((position, viewId, view, holder) -> {
+                switch (viewId) {
+                    case R.id.playlistItem:
+                        playerClient.playPause(position);
+                        break;
+                    case R.id.btnRemove:
+                        playerClient.removeMusicItem(position);
+                        break;
+                }
+            });
+
+            mPlaylistLiveData.observe(PlaylistDialog.this, newPlaylist -> {
+                tvPlaylistTitle.setText(getText(R.string.playlist) + "(" + playlist.size() + ")");
+                mPlaylistAdapter.setPlaylist(newPlaylist);
+            });
+
+            mPlayerViewModel.getPlayPosition()
+                    .observe(PlaylistDialog.this, playPosition -> mPlaylistAdapter.setPlayPosition(playPosition));
+        });
     }
 
     private static class PlaylistAdapter extends RecyclerView.Adapter<PlaylistAdapter.ViewHolder> {
         private static final int TYPE_EMPTY_VIEW = 1;
+        private static final int TYPE_ITEM_VIEW = 2;
         private Playlist mPlaylist;
+
+        private ItemClickHelper mItemClickHelper;
+        private SelectableHelper mSelectableHelper;
         private int mPlayPosition;
 
         public PlaylistAdapter(@NonNull Playlist playlist, int playPosition) {
             Preconditions.checkNotNull(playlist);
             mPlaylist = playlist;
+
+            mItemClickHelper = new ItemClickHelper();
+            mSelectableHelper = new SelectableHelper(this);
             mPlayPosition = playPosition;
-            // TODO update play position
+
+            if (mPlaylist.isEmpty()) {
+                return;
+            }
+
+            mSelectableHelper.setSelect(playPosition, true);
         }
 
         public void setPlaylist(@NonNull Playlist playlist) {
             Preconditions.checkNotNull(playlist);
+
+            if (playlist.isEmpty()) {
+                mPlaylist = playlist;
+                notifyDataSetChanged();
+                mSelectableHelper.clearSelected();
+                return;
+            }
+
+            if (mPlaylist.isEmpty()) {
+                mPlaylist = playlist;
+                notifyDataSetChanged();
+                mSelectableHelper.setSelect(mPlayPosition, true);
+                return;
+            }
+
+            DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new MusicItemDiffCallback(mPlaylist, playlist));
+            diffResult.dispatchUpdatesTo(this);
+
             mPlaylist = playlist;
-            // TODO DiffUtil
-            notifyDataSetChanged();
+            mSelectableHelper.setSelect(mPlayPosition, true);
         }
 
         public void setPlayPosition(int playPosition) {
             mPlayPosition = playPosition;
-            // TODO update play position
+            if (mPlaylist.isEmpty()) {
+                return;
+            }
+
+            mSelectableHelper.setSelect(playPosition, true);
+        }
+
+        public void setOnItemClickListener(ItemClickHelper.OnItemClickListener listener) {
+            mItemClickHelper.setOnItemClickListener(listener);
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+            mItemClickHelper.attachToRecyclerView(recyclerView);
+            mSelectableHelper.attachToRecyclerView(recyclerView);
         }
 
         @NonNull
@@ -149,6 +218,9 @@ public class PlaylistDialog extends AppCompatDialogFragment {
 
             MusicItem musicItem = mPlaylist.get(position);
             holder.tvTitle.setText(musicItem.getTitle());
+
+            mItemClickHelper.bindClickListener(holder.itemView, holder.btnRemove);
+            mSelectableHelper.updateSelectState(holder, position);
         }
 
         @Override
@@ -166,20 +238,89 @@ public class PlaylistDialog extends AppCompatDialogFragment {
                 return TYPE_EMPTY_VIEW;
             }
 
-            return super.getItemViewType(position);
+            return TYPE_ITEM_VIEW;
         }
 
-        public static class ViewHolder extends RecyclerView.ViewHolder {
-            public TextView tvTitle;
+        private static class ViewHolder extends RecyclerView.ViewHolder implements SelectableHelper.Selectable {
+            private boolean mEmptyView;
+            private int mColorMark;
+            private int mColorText;
+
+            View mark;
+            TextView tvTitle;
+            ImageButton btnRemove;
 
             public ViewHolder(@NonNull View itemView, boolean emptyView) {
                 super(itemView);
 
+                mEmptyView = emptyView;
                 if (emptyView) {
                     return;
                 }
 
+                mark = itemView.findViewById(R.id.mark);
                 tvTitle = itemView.findViewById(R.id.tvTitle);
+                btnRemove = itemView.findViewById(R.id.btnRemove);
+
+                Resources res = itemView.getResources();
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    mColorMark = res.getColor(R.color.colorMark, itemView.getContext().getTheme());
+                    mColorText = res.getColor(R.color.colorText, itemView.getContext().getTheme());
+                } else {
+                    mColorMark = res.getColor(R.color.colorMark);
+                    mColorText = res.getColor(R.color.colorText);
+                }
+            }
+
+            @Override
+            public void onSelected() {
+                if (mEmptyView) {
+                    return;
+                }
+
+                mark.setVisibility(View.VISIBLE);
+                tvTitle.setTextColor(mColorMark);
+            }
+
+            @Override
+            public void onUnselected() {
+                if (mEmptyView) {
+                    return;
+                }
+
+                mark.setVisibility(View.GONE);
+                tvTitle.setTextColor(mColorText);
+            }
+        }
+
+        private static class MusicItemDiffCallback extends DiffUtil.Callback {
+            private Playlist mOldPlaylist;
+            private Playlist mNewPlaylist;
+
+            MusicItemDiffCallback(Playlist oldPlaylist, Playlist newPlaylist) {
+                mOldPlaylist = oldPlaylist;
+                mNewPlaylist = newPlaylist;
+            }
+
+            @Override
+            public int getOldListSize() {
+                return mOldPlaylist.size();
+            }
+
+            @Override
+            public int getNewListSize() {
+                return mNewPlaylist.size();
+            }
+
+            @Override
+            public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+                return mOldPlaylist.get(oldItemPosition).equals(mNewPlaylist.get(newItemPosition));
+            }
+
+            @Override
+            public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+                return areItemsTheSame(oldItemPosition, newItemPosition);
             }
         }
     }
