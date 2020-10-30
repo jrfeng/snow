0916bdc452da2b94984c6d1257314921a1d66c03
jrfeng <jrfeng331@qq.com>
2020-10-30 18:10:00 +0800

    update docs

--- player/src/main/java/snow/player/annotation/PersistenceId.java
+++ player/src/main/java/snow/player/annotation/PersistenceId.java
@@ -5,6 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 用于设置 {@link snow.player.PlayerService} 的持久化 ID。
+ * <p>
+ * 该 ID 值将用于播放器状态的持久化，不能为空，并且请保证其全局唯一性。
+ * <p>
+ * 例：
+ * <pre>{@code @PersistenceId("YourPersistenceId")
+ * public class MyPlayerService extends PlayerService {
+ *     ...
+ * }
+ * }
+ * </pre>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface PersistenceId {
