b209e2d125d6b9a7a32b647b10ef7cfafe0e70fa
jrfeng <jrfeng331@qq.com>
2020-11-01 10:45:36 +0800

    add 'BottomBarFragment'

---
+++ app/src/main/java/snow/music/fragment/battombar/BottomBarFragment.java
@@ -0,0 +1,81 @@
+package snow.music.fragment.battombar;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+import java.util.Objects;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import snow.music.R;
+import snow.music.databinding.FragmentBottomBarBinding;
+import snow.music.util.DimenUtil;
+import snow.music.util.MusicUtil;
+import snow.player.lifecycle.PlayerViewModel;
+
+public class BottomBarFragment extends Fragment {
+    private FragmentBottomBarBinding mBinding;
+    private BottomBarViewModel mBottomBarViewModel;
+
+    private Disposable mIconLoadDisposable;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        FragmentActivity activity = getActivity();
+        assert activity != null;
+        ViewModelProvider viewModelProvider = new ViewModelProvider(activity);
+
+        mBottomBarViewModel = viewModelProvider.get(BottomBarViewModel.class);
+        mBottomBarViewModel.init(viewModelProvider.get(PlayerViewModel.class));
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mBinding = FragmentBottomBarBinding.inflate(inflater);
+        mBinding.setBottomBarViewModel(mBottomBarViewModel);
+        mBinding.setLifecycleOwner(this);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cancelLoadMusicIcon();
+    }
+
+    private void loadMusicIcon(String musicUri) {
+        cancelLoadMusicIcon();
+        mIconLoadDisposable = MusicUtil.getEmbeddedPicture(Objects.requireNonNull(getContext()), musicUri)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(bytes -> Glide.with(BottomBarFragment.this)
+                        .load(bytes)
+                        .error(R.mipmap.ic_bottom_bar_default_icon)
+                        .transform(new RoundedCorners(DimenUtil.getDimenPx(getResources(), R.dimen.player_bottom_bar_icon_corner_size)))
+                        .transition(DrawableTransitionOptions.withCrossFade(200))
+                        .into(mBinding.ivIcon));
+    }
+
+    private void cancelLoadMusicIcon() {
+        if (mIconLoadDisposable != null && !mIconLoadDisposable.isDisposed()) {
+            mIconLoadDisposable.dispose();
+        }
+    }
+}
---
+++ app/src/main/java/snow/music/fragment/battombar/BottomBarViewModel.java
@@ -0,0 +1,87 @@
+package snow.music.fragment.battombar;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.arch.core.util.Function;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+import com.google.common.base.Preconditions;
+
+import snow.music.R;
+import snow.music.dialog.PlaylistDialog;
+import snow.player.PlaybackState;
+import snow.player.lifecycle.PlayerViewModel;
+
+public class BottomBarViewModel extends ViewModel {
+    private static final String TAG = "PlayerBottomBar";
+    private PlayerViewModel mPlayerViewModel;
+    private boolean mInitialized;
+
+    public void init(@NonNull PlayerViewModel playerViewModel) {
+        Preconditions.checkNotNull(playerViewModel);
+
+        if (mInitialized) {
+            return;
+        }
+
+        mInitialized = true;
+        mPlayerViewModel = playerViewModel;
+    }
+
+    public LiveData<String> getMusicTitle() {
+        if (!mInitialized) {
+            throw new IllegalStateException("PlayerBottomBarViewModel not init yet.");
+        }
+
+        return mPlayerViewModel.getTitle();
+    }
+
+    public LiveData<String> getMusicArtist() {
+        if (!mInitialized) {
+            throw new IllegalStateException("PlayerBottomBarViewModel not init yet.");
+        }
+
+        return mPlayerViewModel.getArtist();
+    }
+
+    public LiveData<Integer> getPlayPauseDrawableRes() {
+        if (!mInitialized) {
+            throw new IllegalStateException("PlayerBottomBarViewModel not init yet.");
+        }
+
+        return Transformations.map(mPlayerViewModel.getPlaybackState(), playbackState -> {
+            if (playbackState == PlaybackState.PLAYING) {
+                return R.drawable.ic_bottom_bar_pause;
+            } else {
+                return R.drawable.ic_bottom_bar_play;
+            }
+        });
+    }
+
+    public void playPause() {
+        if (!mInitialized) {
+            throw new IllegalStateException("PlayerBottomBarViewModel not init yet.");
+        }
+
+        mPlayerViewModel.playPause();
+    }
+
+    public void showPlaylist(View view) {
+        Preconditions.checkNotNull(view);
+
+        Context context = view.getContext();
+        if (!(context instanceof FragmentActivity)) {
+            Log.e(TAG, "This view not belong a FragmentActivity.");
+            return;
+        }
+
+        PlaylistDialog.newInstance()
+                .show(((FragmentActivity) context).getSupportFragmentManager(), "PlaylistDialog");
+    }
+}
