1b6e0163e50c3dc6a1760d729fd57c17954f5c4c
jrfeng <jrfeng331@qq.com>
2020-11-04 18:25:09 +0800

    add Music option menu & optimize some layout

--- app/src/main/java/snow/music/activity/localmusic/LocalMusicActivity.java
+++ app/src/main/java/snow/music/activity/localmusic/LocalMusicActivity.java
@@ -5,8 +5,10 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,9 +17,12 @@ import io.reactivex.Single;
 import io.reactivex.SingleOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 import snow.music.R;
 import snow.music.adapter.MusicListAdapter;
+import snow.music.dialog.BottomMenuDialog;
+import snow.music.dialog.MessageDialog;
 import snow.music.service.AppPlayerService;
 import snow.music.store.Music;
 import snow.music.store.MusicStore;
@@ -32,6 +37,7 @@ public class LocalMusicActivity extends AppCompatActivity {
     private Disposable mGetAllMusicDisposable;
 
     private LocalMusicViewModel mLocalMusicViewModel;
+    private Disposable mCheckFavoriteDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +71,11 @@ public class LocalMusicActivity extends AppCompatActivity {
         }
     }
 
-    public void onActionMenuClicked(View view) {
+    public void finishSelf(View view) {
+        finish();
+    }
+
+    public void onOptionMenuClicked(View view) {
         // TODO
     }
 
@@ -122,7 +132,132 @@ public class LocalMusicActivity extends AppCompatActivity {
     }
 
     private void onItemMenuClicked(int position) {
-        // TODO show option menu
+        if (mCheckFavoriteDisposable != null) {
+            mCheckFavoriteDisposable.dispose();
+        }
+
+        final Music music = mLocalMusicViewModel.getMusicList().get(position);
+        mCheckFavoriteDisposable = Single.create((SingleOnSubscribe<Boolean>) emitter -> {
+            boolean result = MusicStore.getInstance().isFavorite(music);
+            if (emitter.isDisposed()) {
+                return;
+            }
+
+            emitter.onSuccess(result);
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(showMusicOptionMenu(music));
+    }
+
+    private Consumer<Boolean> showMusicOptionMenu(final Music music) {
+        return favorite -> {
+            int drawableId = R.drawable.ic_menu_item_favorite_false;
+            int stringId = R.string.menu_item_add_to_favorite;
+            if (favorite) {
+                drawableId = R.drawable.ic_menu_item_favorite_true;
+                stringId = R.string.menu_item_remove_from_favorite;
+            }
+
+            BottomMenuDialog dialog = new BottomMenuDialog.Builder(getApplicationContext())
+                    .setTitle(music.getTitle())
+                    .addMenuItem(R.drawable.ic_menu_item_next_play, R.string.menu_item_next_play)
+                    .addMenuItem(drawableId, stringId)
+                    .addMenuItem(R.drawable.ic_menu_item_add, R.string.menu_item_add_to_music_list)
+                    .addMenuItem(R.drawable.ic_menu_item_rington, R.string.menu_item_set_as_ringtone)
+                    .addMenuItem(R.drawable.ic_menu_item_remove, R.string.menu_item_remove)
+                    .setOnMenuItemClickListener((dialog2, clickedPosition) -> {
+                        dialog2.dismiss();
+                        onMusicOptionMenuClicked(clickedPosition, music, favorite);
+                    })
+                    .build();
+
+            dialog.show(getSupportFragmentManager(), "MusicOptionMenu");
+        };
+    }
+
+    private void onMusicOptionMenuClicked(int clickedPosition, Music music, boolean favorite) {
+        switch (clickedPosition) {
+            case 0:
+                setNextPlay(music);
+                break;
+            case 1:
+                if (favorite) {
+                    showRemoveFromFavoriteDialog(music);
+                } else {
+                    addToFavorite(music);
+                }
+                break;
+            case 2:
+                addToMusicList(music);
+                break;
+            case 3:
+                setAsRingtone(music);
+                break;
+            case 4:
+                showRemoveMusicDialog(music);
+                break;
+        }
+    }
+
+    private void setNextPlay(Music music) {
+        mPlayerViewModel.setNextPlay(MusicUtil.asMusicItem(music));
+        Toast.makeText(getApplicationContext(), R.string.toast_added_to_playlist, Toast.LENGTH_SHORT).show();
+    }
+
+    @SuppressLint("CheckResult")
+    private void addToFavorite(Music music) {
+        Single.create((SingleOnSubscribe<Boolean>) emitter -> {
+            MusicStore.getInstance()
+                    .addToFavorite(music);
+            emitter.onSuccess(true);
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(aBoolean -> Toast.makeText(getApplicationContext(), R.string.toast_added_to_favorites, Toast.LENGTH_SHORT).show());
+    }
+
+    private void showRemoveFromFavoriteDialog(Music music) {
+        MessageDialog messageDialog = new MessageDialog.Builder(getApplicationContext())
+                .setTitle(music.getTitle())
+                .setMessage(R.string.message_remove_from_favorite)
+                .setPositiveTextColor(getResources().getColor(R.color.red_500))
+                .setPositiveButtonClickListener((dialog, which) -> removeFromFavorite(music))
+                .build();
+
+        messageDialog.show(getSupportFragmentManager(), "removeFromFavorite");
+    }
+
+    @SuppressLint("CheckResult")
+    private void removeFromFavorite(Music music) {
+        Single.create((SingleOnSubscribe<Boolean>) emitter -> {
+            MusicStore.getInstance()
+                    .removeFromFavorite(music);
+            emitter.onSuccess(true);
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(aBoolean -> Toast.makeText(getApplicationContext(), R.string.toast_removed_from_favorites, Toast.LENGTH_SHORT).show());
+    }
+
+    private void addToMusicList(Music music) {
+        // TODO
+    }
+
+    private void setAsRingtone(Music music) {
+        // TODO
+    }
+
+    private void showRemoveMusicDialog(Music music) {
+        MessageDialog messageDialog = new MessageDialog.Builder(getApplicationContext())
+                .setTitle(music.getTitle())
+                .setMessage(R.string.message_remove_music)
+                .setPositiveTextColor(getResources().getColor(R.color.red_500))
+                .setPositiveButtonClickListener((dialog, which) -> removeMusic(music))
+                .build();
+
+        messageDialog.show(getSupportFragmentManager(), "RemoveMusic");
+    }
+
+    private void removeMusic(Music music) {
+        // TODO
     }
 
     private void getFreshPlaylist(Runnable task) {
