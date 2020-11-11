3e193d7cfc3f82c074fab70b2e35da2526ccd49c
jrfeng <jrfeng331@qq.com>
2020-11-11 20:57:38 +0800

    update docs

--- app/src/main/java/snow/music/util/DimenUtil.java
+++ app/src/main/java/snow/music/util/DimenUtil.java
@@ -7,11 +7,21 @@ import androidx.annotation.NonNull;
 
 import com.google.common.base.Preconditions;
 
+/**
+ * 尺寸工具。
+ */
 public final class DimenUtil {
     private DimenUtil() {
         throw new AssertionError();
     }
 
+    /**
+     * 将 dimen 资源转换成 px 像素值。
+     *
+     * @param resources Resources 对象
+     * @param dimenRes  dimen 资源
+     * @return dimen 资源对应的 px 像素
+     */
     public static int getDimenPx(@NonNull Resources resources, @DimenRes int dimenRes) {
         Preconditions.checkNotNull(resources);
         return resources.getDimensionPixelSize(dimenRes);
--- app/src/main/java/snow/music/util/MusicListUtil.java
+++ app/src/main/java/snow/music/util/MusicListUtil.java
@@ -10,11 +10,23 @@ import snow.music.store.Music;
 import snow.music.store.MusicList;
 import snow.player.playlist.Playlist;
 
+/**
+ * {@link MusicList} 工具，用于将 {@link MusicList} 转换成一个 {@link Playlist} 对象。
+ */
 public final class MusicListUtil {
     private MusicListUtil() {
         throw new AssertionError();
     }
 
+    /**
+     * 将 {@link MusicList} 对象转换成一个 {@link Playlist} 对象。
+     * <p>
+     * 注意！如果 {@link MusicList} 的尺寸大于 {@link Playlist#MAX_SIZE}，则超出部分会被丢弃。
+     *
+     * @param musicList {@link MusicList} 对象，不能为 null。
+     * @return {@link Playlist} 对象，不为 null
+     */
+    @NonNull
     public static Playlist asPlaylist(@NonNull MusicList musicList) {
         Preconditions.checkNotNull(musicList);
 
@@ -28,6 +40,16 @@ public final class MusicListUtil {
         return builder.build();
     }
 
+    /**
+     * 将一个 {@code List<Music>} 列表转换成一个 {@link Playlist} 对象。
+     * <p>
+     * 注意！如果 {@code List<Music>} 列表的尺寸大于 {@link Playlist#MAX_SIZE}，则超出部分会被丢弃。
+     *
+     * @param name       String 对象，该 String 对象将作为 {@link Playlist} 的名称，不能为 null。
+     * @param musicItems {@code List<Music>} 列表，不能为 null。
+     * @return {@link Playlist} 对象，不为 null
+     */
+    @NonNull
     public static Playlist asPlaylist(@NonNull String name, @NonNull List<Music> musicItems) {
         Preconditions.checkNotNull(name);
         Preconditions.checkNotNull(musicItems);
