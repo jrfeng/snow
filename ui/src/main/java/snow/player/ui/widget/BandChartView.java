942c86dddb8b1d7f4aad6dd0f180d2a362719eac
jrfeng <jrfeng331@qq.com>
2020-12-11 17:45:42 +0800

    rename: 'LineChartView' -> 'BandChartView'

--- ui/src/main/java/snow/player/ui/widget/BandChartView.java
+++ ui/src/main/java/snow/player/ui/widget/BandChartView.java
@@ -6,17 +6,17 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
-public class LineChartView extends View {
+public class BandChartView extends View {
 
-    public LineChartView(Context context) {
+    public BandChartView(Context context) {
         super(context);
     }
 
-    public LineChartView(Context context, @Nullable AttributeSet attrs) {
+    public BandChartView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public LineChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public BandChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
