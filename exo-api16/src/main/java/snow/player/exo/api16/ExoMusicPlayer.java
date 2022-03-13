83cc95f38acbb19211dfc61757ba39e1e413d28f
jrfeng <jrfeng331@qq.com>
2022-03-13 11:29:06 +0800

    create module: exo-api16
    Based on ExoPlayer 2.16.1, minSdkVersion 16

---
+++ exo-api16/src/main/java/snow/player/exo/api16/ExoMusicPlayer.java
@@ -0,0 +1,354 @@
+package snow.player.exo.api16;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.PlaybackException;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.MediaSourceFactory;
+
+import snow.player.audio.AbstractMusicPlayer;
+import snow.player.audio.ErrorCode;
+
+/**
+ * 封装了一个 SimpleExoPlayer
+ */
+public class ExoMusicPlayer extends AbstractMusicPlayer {
+    private static final String TAG = "ExoMusicPlayer";
+
+    private ExoPlayer mExoPlayer;
+    private Player.Listener mEventListener;
+
+    @Nullable
+    private OnPreparedListener mPreparedListener;
+    @Nullable
+    private OnCompletionListener mCompletionListener;
+    @Nullable
+    private OnSeekCompleteListener mSeekCompleteListener;
+    @Nullable
+    private OnStalledListener mStalledListener;
+    @Nullable
+    private OnBufferingUpdateListener mBufferingUpdateListener;
+    @Nullable
+    private OnErrorListener mErrorListener;
+    @Nullable
+    private OnRepeatListener mRepeatListener;
+
+    private boolean mStalled;
+    private boolean mInvalid;
+
+    private boolean mPreparing;
+    private boolean mPlayerReady;
+
+    /**
+     * 创建一个 {@link ExoMusicPlayer} 对象。
+     *
+     * @param context            Context 对象，不能为 null
+     * @param mediaSourceFactory MediaSourceFactory 对象，不能为 null
+     * @param uri                要播放的 Uri，不能为 null
+     */
+    public ExoMusicPlayer(@NonNull Context context, @NonNull MediaSourceFactory mediaSourceFactory, @NonNull Uri uri) {
+        initEventListener();
+        initExoPlayer(context);
+
+        MediaSource mediaSource = mediaSourceFactory.createMediaSource(MediaItem.fromUri(uri));
+        mExoPlayer.setMediaSource(mediaSource);
+    }
+
+    /**
+     * 创建一个 {@link ExoMusicPlayer} 对象。
+     *
+     * @param context Context 对象，不能为 null
+     * @param uri     要播放的 Uri，不能为 null
+     */
+    public ExoMusicPlayer(@NonNull Context context, @NonNull Uri uri) {
+        this(context, MediaItem.fromUri(uri));
+    }
+
+    /**
+     * 创建一个 {@link ExoMusicPlayer} 对象。
+     *
+     * @param context   Context 对象，不能为 null
+     * @param mediaItem 要播放的 MediaItem，不能为 null
+     */
+    public ExoMusicPlayer(@NonNull Context context, @NonNull MediaItem mediaItem) {
+        initEventListener();
+        initExoPlayer(context);
+
+        mExoPlayer.setMediaItem(mediaItem);
+    }
+
+    private void initEventListener() {
+        mEventListener = new Player.Listener() {
+            @Override
+            public void onIsLoadingChanged(boolean isLoading) {
+                if (mBufferingUpdateListener != null) {
+                    mBufferingUpdateListener.onBufferingUpdate(ExoMusicPlayer.this,
+                            (int) mExoPlayer.getBufferedPosition(),
+                            false);
+                }
+            }
+
+            @Override
+            public void onPlaybackStateChanged(int state) {
+                switch (state) {
+                    case Player.STATE_READY:
+                        onReady();
+                        break;
+                    case Player.STATE_BUFFERING:
+                        setStalled(true);
+                        break;
+                    case Player.STATE_ENDED:
+                        onEnd();
+                        break;
+                    case Player.STATE_IDLE:
+                        // ignore
+                        break;
+                }
+            }
+
+            private void onReady() {
+                mPlayerReady = true;
+                tryNotifyPrepared();
+
+                if (isStalled()) {
+                    setStalled(false);
+                }
+            }
+
+            private void onEnd() {
+                if (mCompletionListener != null) {
+                    mCompletionListener.onCompletion(ExoMusicPlayer.this);
+                }
+            }
+
+            @Override
+            public void onPlayerError(@NonNull PlaybackException error) {
+                setInvalid();
+
+                Log.e(TAG, error.toString());
+                error.printStackTrace();
+
+                if (mErrorListener != null) {
+                    mErrorListener.onError(ExoMusicPlayer.this, toErrorCode(error));
+                }
+            }
+
+            @Override
+            public void onPositionDiscontinuity(
+                    @NonNull Player.PositionInfo oldPosition,
+                    @NonNull Player.PositionInfo newPosition,
+                    int reason
+            ) {
+                if (reason == Player.DISCONTINUITY_REASON_SEEK
+                        && mSeekCompleteListener != null) {
+                    mSeekCompleteListener.onSeekComplete(ExoMusicPlayer.this);
+                }
+            }
+
+            @Override
+            public void onMediaItemTransition(@Nullable MediaItem mediaItem, int reason) {
+                if (reason == Player.MEDIA_ITEM_TRANSITION_REASON_REPEAT
+                        && mRepeatListener != null) {
+                    mRepeatListener.onRepeat(ExoMusicPlayer.this);
+                }
+            }
+        };
+    }
+
+    private void setStalled(boolean stalled) {
+        mStalled = stalled;
+        if (isPrepared() && mStalledListener != null) {
+            mStalledListener.onStalled(mStalled);
+        }
+    }
+
+    @SuppressLint("SwitchIntDef")
+    private int toErrorCode(PlaybackException error) {
+        if (!(error instanceof ExoPlaybackException)) {
+            return ErrorCode.UNKNOWN_ERROR;
+        }
+
+        switch (((ExoPlaybackException) error).type) {
+            case ExoPlaybackException.TYPE_SOURCE:
+                return ErrorCode.DATA_LOAD_FAILED;
+            case ExoPlaybackException.TYPE_REMOTE:
+                return ErrorCode.NETWORK_ERROR;
+            case ExoPlaybackException.TYPE_RENDERER:
+                return ErrorCode.PLAYER_ERROR;
+            default:
+                return ErrorCode.UNKNOWN_ERROR;
+        }
+    }
+
+    private void initExoPlayer(Context context) {
+        mExoPlayer = new ExoPlayer.Builder(context)
+                .setLooper(Looper.getMainLooper())
+                .build();
+
+        mExoPlayer.addListener(mEventListener);
+    }
+
+    private void tryNotifyPrepared() {
+        if (mPreparing && isPrepared()) {
+            mPreparing = false;
+            onPrepared();
+        }
+    }
+
+    private boolean isPrepared() {
+        return mPlayerReady;
+    }
+
+    private void onPrepared() {
+        if (mPreparedListener != null) {
+            mPreparedListener.onPrepared(ExoMusicPlayer.this);
+        }
+
+        if (mStalled && mStalledListener != null) {
+            mStalledListener.onStalled(true);
+        }
+    }
+
+    @Override
+    public void prepare() {
+        if (isInvalid()) {
+            return;
+        }
+
+        mPreparing = true;
+        mExoPlayer.prepare();
+    }
+
+    @Override
+    public void setLooping(boolean looping) {
+        if (looping) {
+            mExoPlayer.setRepeatMode(Player.REPEAT_MODE_ONE);
+            return;
+        }
+
+        mExoPlayer.setRepeatMode(Player.REPEAT_MODE_OFF);
+    }
+
+    @Override
+    public boolean isLooping() {
+        return mExoPlayer.getRepeatMode() == Player.REPEAT_MODE_ONE;
+    }
+
+    @Override
+    public boolean isStalled() {
+        return mStalled;
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return isPrepared() && (mExoPlayer.isPlaying() || mExoPlayer.getPlayWhenReady());
+    }
+
+    @Override
+    public int getDuration() {
+        return (int) mExoPlayer.getDuration();
+    }
+
+    @Override
+    public int getProgress() {
+        return (int) mExoPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public void startEx() {
+        mExoPlayer.setPlayWhenReady(true);
+    }
+
+    @Override
+    public void pauseEx() {
+        mExoPlayer.setPlayWhenReady(false);
+    }
+
+    @Override
+    public void stopEx() {
+        mExoPlayer.stop();
+    }
+
+    @Override
+    public void seekTo(int pos) {
+        mExoPlayer.seekTo(pos);
+    }
+
+    @Override
+    public void setVolume(float leftVolume, float rightVolume) {
+        mExoPlayer.setVolume(Math.max(leftVolume, rightVolume));
+    }
+
+    @Override
+    public void setSpeed(float speed) {
+        PlaybackParameters parameters = new PlaybackParameters(speed);
+        mExoPlayer.setPlaybackParameters(parameters);
+    }
+
+    @Override
+    public void releaseEx() {
+        setInvalid();
+        mExoPlayer.release();
+    }
+
+    @Override
+    public synchronized boolean isInvalid() {
+        return mInvalid;
+    }
+
+    private synchronized void setInvalid() {
+        mInvalid = true;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return mExoPlayer.getAudioSessionId();
+    }
+
+    @Override
+    public void setOnPreparedListener(@Nullable OnPreparedListener listener) {
+        mPreparedListener = listener;
+    }
+
+    @Override
+    public void setOnCompletionListener(@Nullable OnCompletionListener listener) {
+        mCompletionListener = listener;
+    }
+
+    @Override
+    public void setOnRepeatListener(@Nullable OnRepeatListener listener) {
+        mRepeatListener = listener;
+    }
+
+    @Override
+    public void setOnSeekCompleteListener(@Nullable OnSeekCompleteListener listener) {
+        mSeekCompleteListener = listener;
+    }
+
+    @Override
+    public void setOnStalledListener(@Nullable OnStalledListener listener) {
+        mStalledListener = listener;
+    }
+
+    @Override
+    public void setOnBufferingUpdateListener(@Nullable OnBufferingUpdateListener listener) {
+        mBufferingUpdateListener = listener;
+    }
+
+    @Override
+    public void setOnErrorListener(@Nullable OnErrorListener listener) {
+        mErrorListener = listener;
+    }
+}
---
+++ exo-api16/src/main/java/snow/player/exo/api16/util/OkHttpUtil.java
@@ -0,0 +1,148 @@
+package snow.player.exo.api16.util;
+
+import android.os.Build;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.ConnectionSpec;
+import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
+
+/**
+ * OkHttp 工具，用于在大于等于 API level 16 小于 API level 22 的 Android 版本中启用 TLS v1.2。
+ * <p>
+ * 要使用此工具，你的项目需要添加对 OKHttp 的依赖，本项目不对外提供 OkHttp 依赖。
+ */
+public final class OkHttpUtil {
+
+    private OkHttpUtil() {
+        throw new AssertionError();
+    }
+
+    public static void enableTls12OnPreLollipop(OkHttpClient.Builder builder) {
+        enableTls12OnPreLollipop(builder, false);
+    }
+
+    /**
+     * Warning! Trust all certificate is unsafe!
+     */
+    public static void enableTls12OnPreLollipop(OkHttpClient.Builder builder, boolean trustAllCertificate) {
+        if (Build.VERSION.SDK_INT >= 16 && Build.VERSION.SDK_INT < 22) {
+            try {
+                SSLContext sc = SSLContext.getInstance("TLSv1.2");
+
+                if (trustAllCertificate) {
+                    TrustManager[] trustAllCerts = getUnsafeTrustManager();
+                    sc.init(null, trustAllCerts, new java.security.SecureRandom());
+                    builder.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()), (X509TrustManager) trustAllCerts[0]);
+                } else {
+                    sc.init(null, null, null);
+                    builder.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()));
+                }
+
+                ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                        .tlsVersions(TlsVersion.TLS_1_2)
+                        .build();
+
+                List<ConnectionSpec> specs = new ArrayList<>();
+                specs.add(cs);
+                specs.add(ConnectionSpec.COMPATIBLE_TLS);
+                specs.add(ConnectionSpec.CLEARTEXT);
+
+                builder.connectionSpecs(specs);
+            } catch (Exception exc) {
+                Log.e("OkHttpUtil", "Error while setting TLS 1.2", exc);
+            }
+        }
+    }
+
+    /**
+     * Warning! This is unsafe!
+     */
+    private static TrustManager[] getUnsafeTrustManager() {
+        return new TrustManager[]{new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[]{};
+            }
+
+            public void checkClientTrusted(X509Certificate[] chain, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] chain, String authType) {
+            }
+        }};
+    }
+
+    /**
+     * Enables TLS v1.2 when creating SSLSockets.
+     * <p/>
+     * For some reason, android supports TLS v1.2 from API 16, but enables it by
+     * default only from API 20.
+     *
+     * @link https://developer.android.com/reference/javax/net/ssl/SSLSocket.html
+     * @see SSLSocketFactory
+     */
+    private static class Tls12SocketFactory extends SSLSocketFactory {
+        private static final String[] TLS_V12_ONLY = {"TLSv1.2"};
+
+        final SSLSocketFactory delegate;
+
+        public Tls12SocketFactory(SSLSocketFactory base) {
+            this.delegate = base;
+        }
+
+        @Override
+        public String[] getDefaultCipherSuites() {
+            return delegate.getDefaultCipherSuites();
+        }
+
+        @Override
+        public String[] getSupportedCipherSuites() {
+            return delegate.getSupportedCipherSuites();
+        }
+
+        @Override
+        public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
+            return patch(delegate.createSocket(s, host, port, autoClose));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port) throws IOException {
+            return patch(delegate.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException {
+            return patch(delegate.createSocket(host, port, localHost, localPort));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress host, int port) throws IOException {
+            return patch(delegate.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort) throws IOException {
+            return patch(delegate.createSocket(address, port, localAddress, localPort));
+        }
+
+        private Socket patch(Socket s) {
+            if (s instanceof SSLSocket) {
+                ((SSLSocket) s).setEnabledProtocols(TLS_V12_ONLY);
+            }
+            return s;
+        }
+    }
+}
