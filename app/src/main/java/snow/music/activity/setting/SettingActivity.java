635063af68e9e8d78007e73f1d63b89754b656d4
jrfeng <jrfeng331@qq.com>
2022-07-20 21:07:28 +0800

    [demo]optimize ignore audio focus

--- app/src/main/java/snow/music/activity/setting/SettingActivity.java
+++ app/src/main/java/snow/music/activity/setting/SettingActivity.java
@@ -1,10 +1,17 @@
 package snow.music.activity.setting;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -22,6 +29,8 @@ public class SettingActivity extends AppCompatActivity {
     private static final int DARK_MODE_ID_FOLLOW_SYSTEM = 1;
     private static final int DARK_MODE_ID_OFF = 2;
     private static final int DARK_MODE_ID_ON = 3;
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     private SettingViewModel mSettingViewModel;
 
@@ -53,6 +62,12 @@ public class SettingActivity extends AppCompatActivity {
 
         mSettingViewModel = provider.get(SettingViewModel.class);
         mSettingViewModel.init(playerViewModel);
+
+        mSettingViewModel.getPlayWithOtherApp().observe(this, aBoolean -> {
+            if (aBoolean) {
+                checkReadPhoneStatePermission();
+            }
+        });
     }
 
     private void findViews() {
@@ -136,6 +151,56 @@ public class SettingActivity extends AppCompatActivity {
         dialog.setCancelable(false);
 
         dialog.show(getSupportFragmentManager(), "PlayWithOtherAppTips");
+    }
+
+    private void checkReadPhoneStatePermission() {
+        if (noReadPhoneStatePermission()) {
+            showRequestReadPhoneStatePermissionDialog();
+        }
+    }
+
+    private boolean noReadPhoneStatePermission() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void showRequestReadPhoneStatePermissionDialog() {
+        MessageDialog dialog = new MessageDialog.Builder(this)
+                .setMessage(R.string.description_read_phone_state_rationale)
+                .setPositiveButtonClickListener((dialog1, which) -> ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_REQUEST_CODE))
+                .build();
+
+        dialog.setCancelable(false);
+
+        dialog.show(getSupportFragmentManager(), "PermissionRationale");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            checkPermissionResult(grantResults);
+        }
+    }
+
+    private void checkPermissionResult(@NonNull int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE)) {
+            showRequestReadPhoneStatePermissionDialog();
+        } else {
+            jumpToSettings();
+        }
+    }
+
+    private void jumpToSettings() {
+        Uri uri = Uri.parse("package:" + getPackageName());
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, uri);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     public void finishSelf(View view) {
